844f1a9766663a11316efe41c347eb538fe97c42
Renzo <renzon@gmail.com>
2022-05-02 19:00:13 -0300

    Solução de exercícios da Python Brasil

--- secao_01_estrutura_sequencial/ex_01_alo_mundo.py
+++ secao_01_estrutura_sequencial/ex_01_alo_mundo.py
@@ -12,4 +12,5 @@ Escreva um programa que mostre a mensagem "Olá Mundo"
 
 def ola_mundo():
     """Escreva aqui em baixo a sua solução"""
+    print('Olá Mundo')
 
--- secao_01_estrutura_sequencial/ex_02_escreva_um_numero.py
+++ secao_01_estrutura_sequencial/ex_02_escreva_um_numero.py
@@ -11,6 +11,8 @@ Faça um Programa que peça um número e então mostre a mensagem O número info
 
 """
 
+
 def escreva_um_numero():
     """Escreva aqui em baixo a sua solução"""
-
+    numero = input('Escreava um número: ')
+    print(f'O número informado foi {numero}')
