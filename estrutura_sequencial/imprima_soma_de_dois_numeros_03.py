b63a4859ef7336e206368b01c29eccc407ee0178
Renzo <renzon@gmail.com>
2022-05-02 19:11:11 -0300

    Doctest do exercício 3.

---
+++ estrutura_sequencial/imprima_soma_de_dois_numeros_03.py
@@ -0,0 +1,17 @@
+"""
+Exercício 03 da sessão de estrutura sequencial da Python Brasil:
+https://wiki.python.org.br/EstruturaSequencial
+
+Faça um Programa que peça dois números inteiros e imprima a soma.
+
+    >>> from estrutura_sequencial import imprima_soma_de_dois_numeros_03
+    >>> numeros =['42', '43']
+    >>> imprima_soma_de_dois_numeros_03.input = lambda k: numeros.pop()
+    >>> imprima_soma_de_dois_numeros_03.imprima_a_soma_de_dois_numeros()
+    A soma dos dois números informados é 85
+
+"""
+
+
+def imprima_a_soma_de_dois_numeros():
+    """Escreva aqui em baixo a sua solução"""
