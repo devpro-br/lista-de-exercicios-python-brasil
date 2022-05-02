a92d91e387b7a27eb2759088c835d16c2b6b929d
Renzo <renzon@gmail.com>
2022-05-02 18:00:45 -0300

    Implementada prova de conceito para primeiro exercício da lista da Python Brasil

---
+++ estrutura-sequencial/alo_mundo_01.py
@@ -0,0 +1,14 @@
+"""
+Exercício 01 da sessão de estrutura sequencial da Python Brasil:
+https://wiki.python.org.br/EstruturaSequencial
+
+Escreva um programa que mostre a mensagem "Olá Mundo"
+
+    >>> ola_mundo()
+    >>> 'Ola Mundo'
+"""
+
+
+def ola_mundo():
+    """Escreva aqui em baixo a sua solução"""
+
