46b277c36e70db5ed97825d9048a353a263a9c28
Renzo <renzon@gmail.com>
2022-05-02 18:42:35 -0300

    Criado exercício 02 e corrido nome de pacote

---
+++ estrutura_sequencial/escreva_um_numero_02.py
@@ -0,0 +1,16 @@
+"""
+Exercício 01 da sessão de estrutura sequencial da Python Brasil:
+https://wiki.python.org.br/EstruturaSequencial
+
+Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+    >>> from estrutura_sequencial import escreva_um_numero_02
+    >>> escreva_um_numero_02.input = lambda k: '42'
+    >>> escreva_um_numero_02.escreva_um_numero()
+    O número informado foi 42
+
+"""
+
+def escreva_um_numero():
+    """Escreva aqui em baixo a sua solução"""
+
