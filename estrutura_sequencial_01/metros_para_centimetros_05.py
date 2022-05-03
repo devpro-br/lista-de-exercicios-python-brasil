3b8dc37a1dd6c382b24196bbbc00d5e440fc3803
Renzo <renzon@gmail.com>
2022-05-03 14:14:22 -0300

    Criada correção para exercício 05 da sessão de Estrutura Sequencial

---
+++ estrutura_sequencial_01/metros_para_centimetros_05.py
@@ -0,0 +1,19 @@
+"""
+Exercício 05 da sessão de estrutura sequencial da Python Brasil:
+https://wiki.python.org.br/EstruturaSequencial
+
+Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+    >>> from estrutura_sequencial_01 import metros_para_centimetros_05
+    >>> metros_para_centimetros_05.input = lambda k: '1'
+    >>> metros_para_centimetros_05.converter_metros_para_centrimetros()
+    Transformando para centímetros dá 100.0 cm
+    >>> metros_para_centimetros_05.input = lambda k: '3.621'
+    >>> metros_para_centimetros_05.converter_metros_para_centrimetros()
+    Transformando para centímetros dá 362.1 cm
+
+"""
+
+
+def converter_metros_para_centrimetros():
+    """Escreva aqui em baixo a sua solução"""
