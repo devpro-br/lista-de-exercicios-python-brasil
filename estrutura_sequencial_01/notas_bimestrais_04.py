ebf82c11a02c0699296c0b5040b791895be93d35
Renzo <renzon@gmail.com>
2022-05-03 08:53:57 -0300

    Criado doctest para exercício 4

---
+++ estrutura_sequencial_01/notas_bimestrais_04.py
@@ -0,0 +1,18 @@
+"""
+Exercício 03 da sessão de estrutura sequencial da Python Brasil:
+https://wiki.python.org.br/EstruturaSequencial
+
+Faça um Programa que peça dois números inteiros e imprima a soma.
+
+    >>> from estrutura_sequencial_01 import notas_bimestrais_04
+    >>> numeros =['7', '8','9','10']
+    >>> notas_bimestrais_04.input = lambda k: numeros.pop()
+    >>> notas_bimestrais_04.calcular_media()
+    A média anual é 8.5
+
+"""
+
+
+def calcular_media():
+    """Escreva aqui em baixo a sua solução"""
+
