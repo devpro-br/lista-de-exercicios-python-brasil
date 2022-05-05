3f99d2ebf1cb133f742ea69779cb79e99e7d8e30
renzo <renzo.nuccitelli@buser.com.br>
2022-05-05 13:44:56 -0300

    Resolvido exercício 13 da sessão de estrutura sequencial

--- secao_01_estrutura_sequencial/ex_13_peso_ideal_mulher_e_homem.py
+++ secao_01_estrutura_sequencial/ex_13_peso_ideal_mulher_e_homem.py
@@ -22,3 +22,8 @@ Mostrar a área com 1 casa decimal.
 
 def calcular_peso_ideal():
     """Escreva aqui em baixo a sua solução"""
+    altura = float(input('Digite sua altura em metros: '))
+    peso_ideal_mulher = altura * 62.1 - 44.7
+    peso_ideal_homem = altura * 72.7 - 58
+    print(f'Seu peso ideal é {peso_ideal_mulher:.1f} kg, se você for mulher')
+    print(f'Seu peso ideal é {peso_ideal_homem:.1f} kg, se você for homem')
