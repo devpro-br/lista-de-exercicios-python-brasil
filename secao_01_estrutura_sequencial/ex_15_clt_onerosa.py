dac18cf529f4479677dde16e247885911b56cc62
renzo <renzo.nuccitelli@buser.com.br>
2022-05-05 14:26:39 -0300

    Resolvido exercício 15 da sessão de estrutura sequencial

--- secao_01_estrutura_sequencial/ex_15_clt_onerosa.py
+++ secao_01_estrutura_sequencial/ex_15_clt_onerosa.py
@@ -27,3 +27,15 @@ Mostrar os resultados com duas casas decimais
 
 def calcular_assalto_no_salario():
     """Escreva aqui em baixo a sua solução"""
+    valor_por_hora = float(input('Quanto vocẽ ganha por hora? '))
+    horas_trabalhadas = int(input('Quantas horas você trabalhou esse mês? '))
+    salario_bruto = valor_por_hora * horas_trabalhadas
+    ir = salario_bruto * 0.11
+    inss = salario_bruto * 0.08
+    sindicato = salario_bruto * 0.05
+    salario_liquido = salario_bruto - ir - inss - sindicato
+    print(f'+ Salário Bruto : {salario_bruto:.2f}')
+    print(f'- IR (11%) : R$ {ir:.2f}')
+    print(f'- INSS (8%) : R$ {inss:.2f}')
+    print(f'- Sindicato ( 5%) : R$ {sindicato:.2f}')
+    print(f'= Salário Liquido : R$ {salario_liquido:.2f}')
