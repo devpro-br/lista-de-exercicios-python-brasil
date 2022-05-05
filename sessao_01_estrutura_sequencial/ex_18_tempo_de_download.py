307eee2235bddbce1e5165e36bb1dab198cb3d43
Renzo <renzon@gmail.com>
2022-05-05 20:08:16 -0300

    Criada correção para exercício 18 da sessão de Estrutura Sequencial

---
+++ sessao_01_estrutura_sequencial/ex_18_tempo_de_download.py
@@ -0,0 +1,23 @@
+"""
+Exercício 18 da sessão de estrutura sequencial da Python Brasil:
+https://wiki.python.org.br/EstruturaSequencial
+
+Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps),
+calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
+Arredonde o tempo em minutos
+
+    >>> from sessao_01_estrutura_sequencial import ex_18_tempo_de_download
+    >>> numeros =['50', '2500']
+    >>> ex_18_tempo_de_download.input = lambda k: numeros.pop()
+    >>> ex_18_tempo_de_download.calcular_tempo_de_download()
+    O tempo aproximado do Download é: 7 minuto(s)
+    >>> numeros =['100', '2500']
+    >>> ex_18_tempo_de_download.input = lambda k: numeros.pop()
+    >>> ex_18_tempo_de_download.calcular_tempo_de_download()
+    O tempo aproximado do Download é: 3 minuto(s)
+
+"""
+
+
+def calcular_tempo_de_download():
+    """Escreva aqui em baixo a sua solução"""
