340b54a5ddf9890ba5ecf267db9710533fe97f70
renzo <renzo.nuccitelli@buser.com.br>
2022-05-05 14:41:00 -0300

    Feita correção do exercício 16 da sessão de estrutura sequencial

---
+++ sessao_01_estrutura_sequencial/ex_16_loja_de_tintas_simples.py
@@ -0,0 +1,23 @@
+"""
+Exercício 15 da sessão de estrutura sequencial da Python Brasil:
+https://wiki.python.org.br/EstruturaSequencial
+
+Faça um programa para uma loja de tintas.
+O programa deverá pedir o tamanho em metros quadrados da área a ser pintada.
+Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em latas de
+18 litros, que custam R$ 80,00. Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total.
+
+    >>> from sessao_01_estrutura_sequencial import ex_16_loja_de_tintas_simples
+    >>> ex_16_loja_de_tintas_simples.input = lambda k: '50'
+    >>> ex_16_loja_de_tintas_simples.calcular_latas_e_preco_de_tinta()
+    Vocẽ deve comprar 1 lata(s) tinta ao custo de R$ 80.00
+    >>> ex_16_loja_de_tintas_simples.input = lambda k: '100'
+    >>> ex_16_loja_de_tintas_simples.calcular_latas_e_preco_de_tinta()
+    Vocẽ deve comprar 2 lata(s) tinta ao custo de R$ 160.00
+
+
+"""
+
+
+def calcular_latas_e_preco_de_tinta():
+    """Escreva aqui em baixo a sua solução"""
