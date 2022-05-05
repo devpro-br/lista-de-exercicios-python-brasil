64224232ceef4e6ccf20fac944c5bfa6a08a96bd
renzo <renzo.nuccitelli@buser.com.br>
2022-05-05 09:54:15 -0300

    Criada correção de exercício 08 da Estrutura Sequencial

---
+++ estrutura_sequencial_01/horas_trabalhadas_mes_08.py
@@ -0,0 +1,19 @@
+"""
+Exercício 08 da sessão de estrutura sequencial da Python Brasil:
+https://wiki.python.org.br/EstruturaSequencial
+
+Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
+Calcule e mostre o total do seu salário no referido mês.
+Mostrar salário com duas casas decimais
+
+    >>> from estrutura_sequencial_01 import horas_trabalhadas_mes_08
+    >>> numeros =['80', '55.62']
+    >>> horas_trabalhadas_mes_08.input = lambda k: numeros.pop()
+    >>> horas_trabalhadas_mes_08.calcular_salario()
+    Seu salário desse mês é 4449.60
+
+"""
+
+
+def calcular_salario():
+    """Escreva aqui em baixo a sua solução"""
