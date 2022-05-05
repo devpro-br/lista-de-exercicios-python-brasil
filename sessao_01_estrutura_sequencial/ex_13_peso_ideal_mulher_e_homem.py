b9cccb63bc28a92b6ba7274484bf99c3c3e08b09
renzo <renzo.nuccitelli@buser.com.br>
2022-05-05 13:44:14 -0300

    Feita correção do exercício 13 da sessão de estrutura sequencial

---
+++ sessao_01_estrutura_sequencial/ex_13_peso_ideal_mulher_e_homem.py
@@ -0,0 +1,24 @@
+"""
+Exercício 13 da sessão de estrutura sequencial da Python Brasil:
+https://wiki.python.org.br/EstruturaSequencial
+
+Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas:
+Para homens: (72.7*h) - 58
+Para mulheres: (62.1*h) - 44.7
+Mostra a área com 1 casa decimal.
+
+    >>> from sessao_01_estrutura_sequencial import ex_13_peso_ideal_mulher_e_homem
+    >>> ex_13_peso_ideal_mulher_e_homem.input = lambda k: '1.62'
+    >>> ex_13_peso_ideal_mulher_e_homem.calcular_peso_ideal()
+    Seu peso ideal é 55.9 kg, se você for mulher
+    Seu peso ideal é 59.8 kg, se você for homem
+    >>> ex_13_peso_ideal_mulher_e_homem.input = lambda k: '1.80'
+    >>> ex_13_peso_ideal_mulher_e_homem.calcular_peso_ideal()
+    Seu peso ideal é 67.1 kg, se você for mulher
+    Seu peso ideal é 72.9 kg, se você for homem
+
+"""
+
+
+def calcular_peso_ideal():
+    """Escreva aqui em baixo a sua solução"""
