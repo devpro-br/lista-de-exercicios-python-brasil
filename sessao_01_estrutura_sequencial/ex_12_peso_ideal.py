248fa8192b6066fa6ba6fd04d89350a6e64fe235
renzo <renzo.nuccitelli@buser.com.br>
2022-05-05 13:32:40 -0300

    Feita correção do exercício 12 da sessão de estrutura sequencial

---
+++ sessao_01_estrutura_sequencial/ex_12_peso_ideal.py
@@ -0,0 +1,21 @@
+"""
+Exercício 12 da sessão de estrutura sequencial da Python Brasil:
+https://wiki.python.org.br/EstruturaSequencial
+
+Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte
+fórmula: (72.7*altura) - 58
+Mostra a área com 1 casa decimal.
+
+    >>> from sessao_01_estrutura_sequencial import ex_12_peso_ideal
+    >>> ex_12_peso_ideal.input = lambda k: '1.62'
+    >>> ex_12_peso_ideal.calcular_peso_ideal()
+    Seu peso ideal é 59.8 kg
+    >>> ex_12_peso_ideal.input = lambda k: '1.80'
+    >>> ex_12_peso_ideal.calcular_peso_ideal()
+    Seu peso ideal é 72.9 kg
+
+"""
+
+
+def calcular_peso_ideal():
+    """Escreva aqui em baixo a sua solução"""
