70d761c2909789f710639590ded82fff862c6d4b
renzo <renzo.nuccitelli@buser.com.br>
2022-05-05 09:38:12 -0300

    Feita solução para exercício 07 da Estrutura Sequencial

--- secao_01_estrutura_sequencial/ex_07_area_quadrado.py
+++ secao_01_estrutura_sequencial/ex_07_area_quadrado.py
@@ -20,3 +20,8 @@ Mostrar a área com 2 casas decimais.
 
 def calcular_area_de_quadrado():
     """Escreva aqui em baixo a sua solução"""
+    lado = float(input('Digite o valor de lado do quadrado: '))
+    area = lado ** 2
+    area_em_dobro = area * 2
+    print(f'A área do quadrado com esse lado é: {area:.2f}')
+    print(f'O dobro da aŕea do quadrado é: {area_em_dobro:.2f}')
