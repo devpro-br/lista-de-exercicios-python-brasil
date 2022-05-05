0581d330f8402cc7eedd0a43aff47fa5a7907fcd
renzo <renzo.nuccitelli@buser.com.br>
2022-05-05 09:34:09 -0300

    Adicionado teste de exercício 07 da Estrutura Sequencial

---
+++ estrutura_sequencial_01/area_quadrado_07.py
@@ -0,0 +1,21 @@
+"""
+Exercício 07 da sessão de estrutura sequencial da Python Brasil:
+https://wiki.python.org.br/EstruturaSequencial
+
+Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário. ostra a área com 2 casas decimais.
+
+    >>> from estrutura_sequencial_01 import area_quadrado_07
+    >>> area_quadrado_07.input = lambda k: '2'
+    >>> area_quadrado_07.calcular_area_de_quadrado()
+    A área do quadrado com esse lado é: 4.00
+    O dobro da aŕea do quadrado é: 8.00
+    >>> area_quadrado_07.input = lambda k: '2.5'
+    >>> area_quadrado_07.calcular_area_de_quadrado()
+    A área do quadrado com esse lado é: 6.25
+    O dobro da aŕea do quadrado é: 12.50
+
+"""
+
+
+def calcular_area_de_quadrado():
+    """Escreva aqui em baixo a sua solução"""
