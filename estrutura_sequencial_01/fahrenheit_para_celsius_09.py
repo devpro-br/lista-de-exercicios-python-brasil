678e444e3f309436dbbde17caa47f97dd825e9e8
renzo <renzo.nuccitelli@buser.com.br>
2022-05-05 10:08:26 -0300

    Criada correção de exercício 09 da Estrutura Sequencial

---
+++ estrutura_sequencial_01/fahrenheit_para_celsius_09.py
@@ -0,0 +1,21 @@
+"""
+Exercício 08 da sessão de estrutura sequencial da Python Brasil:
+https://wiki.python.org.br/EstruturaSequencial
+
+Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+C = 5 * ((F-32) / 9)
+Mostrar apenas valor inteiro da temperatura
+
+    >>> from estrutura_sequencial_01 import fahrenheit_para_celsius_09
+    >>> fahrenheit_para_celsius_09.input = lambda k: '0'
+    >>> fahrenheit_para_celsius_09.transformar_para_celsius()
+    Essa temperatura é de -18 Celsius
+    >>> fahrenheit_para_celsius_09.input = lambda k: '70'
+    >>> fahrenheit_para_celsius_09.transformar_para_celsius()
+    Essa temperatura é de 21 Celsius
+
+"""
+
+
+def transformar_para_celsius():
+    """Escreva aqui em baixo a sua solução"""
