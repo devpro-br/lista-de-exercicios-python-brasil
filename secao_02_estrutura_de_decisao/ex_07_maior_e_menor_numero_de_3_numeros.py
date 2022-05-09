241d8dfaaaa02bf082b8fad20295d07f32093011
Renzo <renzon@gmail.com>
2022-05-09 18:41:48 -0300

    Teste para correção do exerício 07 da sessão Estrutura de Decisão.

--- secao_02_estrutura_de_decisao/ex_07_maior_e_menor_numero_de_3_numeros.py
+++ secao_02_estrutura_de_decisao/ex_07_maior_e_menor_numero_de_3_numeros.py
@@ -1,17 +1,21 @@
 """
-Exercício 06 da seção de estrutura de decisão da Python Brasil:
+Exercício 07 da seção de estrutura de decisão da Python Brasil:
 https://wiki.python.org.br/EstruturaDeDecisao
 
-Faça um Programa que leia três números e mostre o maior deles.
+Faça um Programa que leia três números e mostre o maior e o menor deles.
 
     >>> calcular_maior_de_3_numeros(2,3, 5)
-    5
+    Maior: 5
+    Menor: 2
     >>> calcular_maior_de_3_numeros(-1, -10, -2)
-    -1
+    Maior: -1
+    Menor: -10
     >>> calcular_maior_de_3_numeros(-5, 3, 0)
-    3
+    Maior: 3
+    Menor: -5
     >>> calcular_maior_de_3_numeros(7, -14, 15)
-    15
+    Maior: 15
+    Menor: -14
 """
 
 
