1856c371d6badd278222840d0c3c9c6c163ee96d
Renzo <renzon@gmail.com>
2022-05-09 18:37:59 -0300

    Teste para correção do exerício 06 da sessão Estrutura de Decisão.

---
+++ secao_02_estrutura_de_decisao/ex_06_maior_numero_de_3_numeros.py
@@ -0,0 +1,19 @@
+"""
+Exercício 03 da seção de estrutura de decisão da Python Brasil:
+https://wiki.python.org.br/EstruturaDeDecisao
+
+Faça um Programa que leia três números e mostre o maior deles.
+
+    >>> calcular_maior_de_3_numeros(2,3, 5)
+    5
+    >>> calcular_maior_de_3_numeros(-1, -10, -2)
+    -2
+    >>> calcular_maior_de_3_numeros(-5, 3, 0)
+    3
+    >>> calcular_maior_de_3_numeros(7, -14, 15)
+    15
+"""
+
+
+def calcular_maior_de_3_numeros(x, y, z):
+    """Escreva aqui em baixo a sua solução"""
