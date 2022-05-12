40867a5b56c618d9d5c50fe2582f3d5d62ded289
Renzo <renzon@gmail.com>
2022-05-12 09:47:30 -0300

    Teste para correção do exerício 08 da sessão Estrutura de Decisão.

---
+++ secao_02_estrutura_de_decisao/ex_08_escolha_de_produto.py
@@ -0,0 +1,22 @@
+"""
+Exercício 08 da seção de estrutura de decisão da Python Brasil:
+https://wiki.python.org.br/EstruturaDeDecisao
+
+Faça um programa que pergunte o preço de três produtos e informe qual produto você deve comprar, sabendo que a decisão
+é sempre pelo mais barato.
+Mostrar o resultado com duas casas decimais
+
+    >>> decidir_melhor_produto(2, 3, 5)
+    Melhor produto custa R$ 2.00
+    >>> decidir_melhor_produto(10, 5.55, 7)
+    Melhor produto custa R$ 5.55
+    >>> decidir_melhor_produto(20, 30, 17.62)
+    Melhor produto custa R$ 17.62
+    >>> decidir_melhor_produto(7, 1, 15)
+    Melhor produto custa R$ 1.00
+
+"""
+
+
+def decidir_melhor_produto(x, y, z):
+    """Escreva aqui em baixo a sua solução"""
