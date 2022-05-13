5ab12187298e2ec2c600f5390e9b33f3947066fe
renzo <renzo.nuccitelli@buser.com.br>
2022-05-13 09:21:28 -0300

    Feita correção do exercício 09 da seção de estrutura de decisão

---
+++ secao_02_estrutura_de_decisao/ex_09_numeros_decrescentes.py
@@ -0,0 +1,20 @@
+"""
+Exercício 09 da seção de estrutura de decisão da Python Brasil:
+https://wiki.python.org.br/EstruturaDeDecisao
+
+Faça um Programa que leia três números e mostre-os em ordem decrescente.
+
+    >>> ordenar_decrescente(2, 3, 5)
+    5, 3, 2
+    >>> ordenar_decrescente(10, 5.55, 7)
+    10, 7, 5.55
+    >>> ordenar_decrescente(20, 30, 17.62)
+    30, 20, 17.62
+    >>> ordenar_decrescente(7, 1, 15)
+    15, 7, 1
+
+"""
+
+
+def ordenar_decrescente(x, y, z):
+    """Escreva aqui em baixo a sua solução"""
