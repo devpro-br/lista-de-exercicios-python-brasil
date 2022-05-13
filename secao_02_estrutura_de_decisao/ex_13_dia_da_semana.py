02530cbd97bac04dd8a7fb30d01f1c223630d920
renzo <renzo.nuccitelli@buser.com.br>
2022-05-13 15:18:32 -0300

    Feita correção do exercício 13 da seção de estrutura de decisão.

---
+++ secao_02_estrutura_de_decisao/ex_13_dia_da_semana.py
@@ -0,0 +1,31 @@
+"""
+Exercício 13 da seção de estrutura de decisão da Python Brasil:
+https://wiki.python.org.br/EstruturaDeDecisao
+
+Faça um Programa que leia um número e exiba o dia correspondente da semana. (1-Domingo, 2- Segunda, etc.),
+se digitar outro valor deve aparecer valor inválido.
+
+    >>> calcular_dia_da_semana(1)
+    'Domingo'
+    >>> calcular_dia_da_semana(2)
+    'Segunda'
+    >>> calcular_dia_da_semana(3)
+    'Terça'
+    >>> calcular_dia_da_semana(4)
+    'Quarta'
+    >>> calcular_dia_da_semana(5)
+    'Quinta'
+    >>> calcular_dia_da_semana(6)
+    'Sexta'
+    >>> calcular_dia_da_semana(7)
+    'Sábado'
+    >>> calcular_dia_da_semana(8)
+    'Dia Inválido'
+    >>> calcular_dia_da_semana(0)
+    'Dia Inválido'
+
+"""
+
+
+def calcular_dia_da_semana(numero: int):
+    """Escreva aqui em baixo a sua solução"""
