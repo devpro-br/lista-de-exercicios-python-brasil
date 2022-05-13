407173c424bb012864ec3cf4b6caf75ec6b4ced8
renzo <renzo.nuccitelli@buser.com.br>
2022-05-13 09:32:16 -0300

    Feita correção do exercício 10 da seção de estrutura de decisão

---
+++ secao_02_estrutura_de_decisao/ex_10_cumprimento_em_turno.py
@@ -0,0 +1,27 @@
+"""
+Exercício 10 da seção de estrutura de decisão da Python Brasil:
+https://wiki.python.org.br/EstruturaDeDecisao
+
+Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou V-Vespertino ou N- Noturno.
+Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
+
+    >>> cumprimentar('M')
+    'Bom dia!'
+    >>> cumprimentar('m')
+    'Bom dia!'
+    >>> cumprimentar('V')
+    'Boa tarde!'
+    >>> cumprimentar('v')
+    'Boa tarde!'
+    >>> cumprimentar('N')
+    'Boa noite!'
+    >>> cumprimentar('n')
+    'Boa noite!'
+    >>> cumprimentar('X')
+    'Valor Inválido!'
+
+"""
+
+
+def cumprimentar(turno: str):
+    """Escreva aqui em baixo a sua solução"""
