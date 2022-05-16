89e60d6418f19c2433a715e3878dd7761dbfcf65
renzo <renzo.nuccitelli@buser.com.br>
2022-05-16 14:53:16 -0300

    Feita correção do exercício 18 da seção de estrutura de decisão.

---
+++ secao_02_estrutura_de_decisao/ex_18_validador_de_data.py
@@ -0,0 +1,26 @@
+"""
+Exercício 18 da seção de estrutura de decisão da Python Brasil:
+https://wiki.python.org.br/EstruturaDeDecisao
+
+Faça um Programa que peça uma data no formato dd/mm/aaaa e determine se a mesma é uma data válida.
+
+    >>> validar_data('')
+    'Data inválida'
+    >>> validar_data('1')
+    'Data inválida'
+    >>> validar_data('1/2/2004')
+    'Data inválida'
+    >>> validar_data('1/02/2004')
+    'Data inválida'
+    >>> validar_data('01/02/2004')
+    'Data válida'
+    >>> validar_data('30/02/2004')
+    'Data inválida'
+    >>> validar_data('01/13/2004')
+    'Data inválida'
+
+"""
+
+
+def validar_data(data: str):
+    """Escreva aqui em baixo a sua solução"""
