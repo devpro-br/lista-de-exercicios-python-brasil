a09fefdbafa2c2404e6340b95d785a606f8d9bb6
renzo <renzo.nuccitelli@buser.com.br>
2022-05-16 14:14:36 -0300

    Feito gabarito da questão 15 da sessão 02 estrutura de decisão

--- secao_02_estrutura_de_decisao/ex_15_triangulo.py
+++ secao_02_estrutura_de_decisao/ex_15_triangulo.py
@@ -27,3 +27,11 @@ Triângulo Escaleno: três lados diferentes;
 
 def classificar_triangulo(lado_a: float, lado_b: float, lado_c: float):
     """Escreva aqui em baixo a sua solução"""
+    if (lado_b + lado_a) < lado_c or (lado_c + lado_a) < lado_b or (lado_b + lado_c) < lado_a:
+        return 'Não é um triângulo'
+    conjunto_lados = {lado_a, lado_b, lado_c}
+    if len(conjunto_lados) == 1:
+        return 'Triângulo Equilátero'
+    if len(conjunto_lados) == 2:
+        return 'Triângulo Isósceles'
+    return 'Triângulo Escaleno'
