eb74bed7519781d5dbfa42a913973a568c443e32
renzo <renzo.nuccitelli@buser.com.br>
2022-05-16 14:44:33 -0300

    Feita correção do exercício 17 da seção de estrutura de decisão.

---
+++ secao_02_estrutura_de_decisao/ex_17_ano_bissexto.py
@@ -0,0 +1,22 @@
+"""
+Exercício 17 da seção de estrutura de decisão da Python Brasil:
+https://wiki.python.org.br/EstruturaDeDecisao
+
+Faça um Programa que peça um número correspondente a um determinado ano e em seguida informe se este ano é ou não
+bissexto.
+    >>> eh_ano_bissexto(400)
+    True
+    >>> eh_ano_bissexto(800)
+    True
+    >>> eh_ano_bissexto(2100)
+    False
+    >>> eh_ano_bissexto(2004)
+    True
+    >>> eh_ano_bissexto(2022)
+    False
+
+"""
+
+
+def eh_ano_bissexto(ano: int):
+    """Escreva aqui em baixo a sua solução"""
