ac2975f0c4137bcad4a12e4300f30d1cf6f992b6
Renzo <renzon@gmail.com>
2022-05-17 13:56:02 -0300

    Feita correção do exercício 23 da seção de estrutura de decisão.

---
+++ secao_02_estrutura_de_decisao/ex_23_inteiro_ou_decimal.py
@@ -0,0 +1,24 @@
+"""
+Exercício 23 da seção de estrutura de decisão da Python Brasil:
+https://wiki.python.org.br/EstruturaDeDecisao
+
+Faça um Programa que peça um número e informe se o número é inteiro ou decimal. Dica: utilize uma função de
+arredondamento.
+
+    >>> decidir_se_eh_inteiro_ou_decimal(256)
+    'Inteiro'
+    >>> decidir_se_eh_inteiro_ou_decimal(1.0)
+    'Inteiro'
+    >>> decidir_se_eh_inteiro_ou_decimal(2.0000)
+    'Inteiro'
+    >>> decidir_se_eh_inteiro_ou_decimal(2.00001)
+    'Decimal'
+    >>> decidir_se_eh_inteiro_ou_decimal(11.2)
+    'Decimal'
+    >>> decidir_se_eh_inteiro_ou_decimal(3.1415)
+    'Decimal'
+"""
+
+
+def decidir_se_eh_inteiro_ou_decimal(valor) -> str:
+    """Escreva aqui em baixo a sua solução"""
