1769fdc6acff9fb058f8eb3ffc05c7f28a87d883
Renzo <renzon@gmail.com>
2022-05-17 13:59:33 -0300

    Alterados tipo de argumento do exercício inteiro ou decimal para string

--- secao_02_estrutura_de_decisao/ex_23_inteiro_ou_decimal.py
+++ secao_02_estrutura_de_decisao/ex_23_inteiro_ou_decimal.py
@@ -5,20 +5,20 @@ https://wiki.python.org.br/EstruturaDeDecisao
 Faça um Programa que peça um número e informe se o número é inteiro ou decimal. Dica: utilize uma função de
 arredondamento.
 
-    >>> decidir_se_eh_inteiro_ou_decimal(256)
+    >>> decidir_se_eh_inteiro_ou_decimal('256')
     'Inteiro'
-    >>> decidir_se_eh_inteiro_ou_decimal(1.0)
+    >>> decidir_se_eh_inteiro_ou_decimal('1.0')
     'Inteiro'
-    >>> decidir_se_eh_inteiro_ou_decimal(2.0000)
+    >>> decidir_se_eh_inteiro_ou_decimal('2.0000')
     'Inteiro'
-    >>> decidir_se_eh_inteiro_ou_decimal(2.00001)
+    >>> decidir_se_eh_inteiro_ou_decimal('2.00001')
     'Decimal'
-    >>> decidir_se_eh_inteiro_ou_decimal(11.2)
+    >>> decidir_se_eh_inteiro_ou_decimal('11.2')
     'Decimal'
-    >>> decidir_se_eh_inteiro_ou_decimal(3.1415)
+    >>> decidir_se_eh_inteiro_ou_decimal('3.1415')
     'Decimal'
 """
 
 
-def decidir_se_eh_inteiro_ou_decimal(valor) -> str:
+def decidir_se_eh_inteiro_ou_decimal(valor: str) -> str:
     """Escreva aqui em baixo a sua solução"""
