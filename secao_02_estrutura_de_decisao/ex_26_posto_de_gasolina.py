345a555d2e0f3f22b14b6f0a016e29d427ca825b
Renzo <renzon@gmail.com>
2022-05-18 09:22:24 -0300

    Feita correção do exercício 26 da seção de estrutura de decisão.

---
+++ secao_02_estrutura_de_decisao/ex_26_posto_de_gasolina.py
@@ -0,0 +1,37 @@
+"""
+Exercício 26 da seção de estrutura de decisão da Python Brasil:
+https://wiki.python.org.br/EstruturaDeDecisao
+
+Um posto está vendendo combustíveis com a seguinte tabela de descontos:
+
+    Álcool:
+    até 20 litros, desconto de 3% por litro
+    acima de 20 litros, desconto de 5% por litro
+    Gasolina:
+    até 20 litros, desconto de 4% por litro
+    acima de 20 litros, desconto de 6% por litro
+Escreva um algoritmo que leia o número de litros vendidos,
+o tipo de combustível (codificado da seguinte forma: A-álcool, G-gasolina),
+calcule e imprima o valor a ser pago pelo cliente sabendo-se que o preço do litro da gasolina é R$ 2,50 o
+preço do litro do álcool é R$ 1,90.
+
+Mostre o restultado com duas casas decimais
+
+    >>> calcular_abastecimento(10, 'A')
+    '10 litro(s) de álcool custa(m): R$ 19.00. Com 3% de desconto, fica R$ 18.43'
+    >>> calcular_abastecimento(20, 'A')
+    '20 litro(s) de álcool custa(m): R$ 38.00. Com 3% de desconto, fica R$ 36.86'
+    >>> calcular_abastecimento(30, 'A')
+    '30 litro(s) de álcool custa(m): R$ 57.00. Com 5% de desconto, fica R$ 54.86'
+    >>> calcular_abastecimento(10, 'G')
+    '10 litro(s) de gasolina custa(m): R$ 25.00. Com 4% de desconto, fica R$ 24.00'
+    >>> calcular_abastecimento(20, 'G')
+    '20 litro(s) de gasolina custa(m): R$ 50.00. Com 4% de desconto, fica R$ 48.00'
+    >>> calcular_abastecimento(30, 'G')
+    '30 litro(s) de gasolina custa(m): R$ 75.00. Com 6% de desconto, fica R$ 70.50'
+
+"""
+
+
+def calcular_abastecimento(n_1: float, n_2: float, operacao: str):
+    """Escreva aqui em baixo a sua solução"""
