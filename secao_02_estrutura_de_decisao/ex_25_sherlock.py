bd3993b1b75c29ff242a511d7676f5a4c08dfb6e
Renzo <renzon@gmail.com>
2022-05-18 09:09:57 -0300

    Feita solução para exercício 25 da seção de estrutura de decisão

--- secao_02_estrutura_de_decisao/ex_25_sherlock.py
+++ secao_02_estrutura_de_decisao/ex_25_sherlock.py
@@ -33,3 +33,6 @@ Caso contrário, ele será classificado como "Inocente".
 
 def investigar(telefonou: str, estava_no_local: str, mora_perto: str, devia: str, trabalhou: str, ):
     """Escreva aqui em baixo a sua solução"""
+    respostas = (telefonou, estava_no_local, mora_perto, devia, trabalhou)
+    quantidade_de_sim = respostas.count('Sim')
+    return ('Inocente', 'Inocente', 'Suspeito', 'Cúmplice', 'Cúmplice', 'Assassino')[quantidade_de_sim]
