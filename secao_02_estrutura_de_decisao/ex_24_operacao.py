1c42a8d05bdd3580b32ca12bcf2a4a0df859b597
Renzo <renzon@gmail.com>
2022-05-18 08:42:35 -0300

    Feita correção do exercício 24 da seção de estrutura de decisão

---
+++ secao_02_estrutura_de_decisao/ex_24_operacao.py
@@ -0,0 +1,30 @@
+"""
+Exercício 24 da seção de estrutura de decisão da Python Brasil:
+https://wiki.python.org.br/EstruturaDeDecisao
+
+Faça um Programa que leia 2 números e em seguida pergunte ao usuário qual operação ele deseja realizar. O resultado da
+operação deve ser acompanhado de uma frase que diga se o número é:
+  par ou ímpar;
+  positivo ou negativo;
+  inteiro ou decimal.
+
+Mostre o restultado com duas casas decimais
+
+    >>> fazer_operacao_e_classificar(2, 3, '+')
+    Resultado: 5.00
+    Número é impar, positivo e inteiro.
+    >>> fazer_operacao_e_classificar(-2, 2, '/')
+    Resultado: -1.00
+    Número é par, negativo e inteiro.
+    >>> fazer_operacao_e_classificar(-3.14, 2, '*')
+    Resultado: -6.28
+    Número é negativo e decimal.
+    >>> fazer_operacao_e_classificar(3.14, 2, '-')
+    Resultado: 1.14
+    Número é positivo e decimal.
+
+"""
+
+
+def fazer_operacao_e_classificar(n_1: float, n_2: float, operacao: str):
+    """Escreva aqui em baixo a sua solução"""
