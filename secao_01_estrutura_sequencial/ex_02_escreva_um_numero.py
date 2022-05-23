e6472761584c0058a6082532cc7c579b56c52441
Lucca Nunes <luccanrib@gmail.com>
2022-05-23 14:35:05 -0300

    Resolvivo exercicio 1 e 2

--- secao_01_estrutura_sequencial/ex_02_escreva_um_numero.py
+++ secao_01_estrutura_sequencial/ex_02_escreva_um_numero.py
@@ -2,7 +2,7 @@
 Exercício 02 da seção de estrutura sequencial da Python Brasil:
 https://wiki.python.org.br/EstruturaSequencial
 
-Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+Faça um Programa que peça dum número e então mostre a mensagem O número informado foi [número].
 
     >>> from secao_01_estrutura_sequencial import ex_02_escreva_um_numero
     >>> ex_02_escreva_um_numero.input = lambda k: '42'
@@ -12,7 +12,7 @@ Faça um Programa que peça um número e então mostre a mensagem O número info
 """
 
 def escreva_um_numero():
-    num = int(input("Qual numero você deseja? "))
-    print(f'O número informado foi {num}')
+    num = int(input("Qual numero você deseja?  "))
+    print(f'O número informado foi {num} ')
 
 
