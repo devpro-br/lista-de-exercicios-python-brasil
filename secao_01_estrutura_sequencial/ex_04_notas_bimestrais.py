612eb7c90bf5a8dcf2fe545df9e1eb515f29af8e
Marcelo Silva <45953439+mhenrique94@users.noreply.github.com>
2022-05-24 10:54:57 -0300

    Resolvido exercício 5

--- secao_01_estrutura_sequencial/ex_04_notas_bimestrais.py
+++ secao_01_estrutura_sequencial/ex_04_notas_bimestrais.py
@@ -16,13 +16,18 @@ def calcular_media():
 
 >>>>>>> 79c3788 (Resolvido exercio 04)
     """Escreva aqui em baixo a sua solução"""
+    # print("Calculo de média por meio de 4 notas.\n")
     num1 = int(input("Digite a primeira nota: "))
     num2 = int(input("Digite a segunda nota: "))
     num3 = int(input("Digite a terceira nota: "))
     num4 = int(input("Digite a quarta nota: "))
     media = (num1+num2+num3+num4)/4
 <<<<<<< HEAD
+<<<<<<< HEAD
     print("A média anual é", media)
 =======
     print("A média anual é", media)
 >>>>>>> 79c3788 (Resolvido exercio 04)
+=======
+    print("A média anual é", media)
+>>>>>>> 7f7e747 (Resolvido exercício 5)
