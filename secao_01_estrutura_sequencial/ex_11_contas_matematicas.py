bcca7c86d1f4b2c180c47ab665ec85a3db806102
Daniel Dantas da Silva <daniel.dantas.98@gmail.com>
2022-05-24 12:45:19 -0300

    Feito até o exercicio 12

--- secao_01_estrutura_sequencial/ex_11_contas_matematicas.py
+++ secao_01_estrutura_sequencial/ex_11_contas_matematicas.py
@@ -22,5 +22,14 @@ Faça um Programa que peça 2 números inteiros e um número real. Calcule e mos
 
 def calcular_formulas():
     """Escreva aqui em baixo a sua solução"""
+    numero1 = int(input('Digite o primeiro número inteiro'))
+    numero2 = int(input('Digite o segundo número inteiro'))
+    numero3 = float(input('Digite o número real'))
+    resposta1 = float((numero1*2)*(numero2/2))
+    resposta2 = float((numero1*3)+(numero3))
+    resposta3 = float(numero3*numero3*numero3)
+    print('O produto do dobro do primeiro com metade do segundo é %.2f' % resposta1)
+    print('A soma do triplo do primeiro com o terceiro é %.2f' % resposta2)
+    print('O terceiro elevado ao cubo é %.2f' % resposta3)
 
 
