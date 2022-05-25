c7f813aa7a5863f1dc40f93f1f0ea4b7948879d0
DavidSilveira80 <davidpscontato@gmail.com>
2022-05-25 20:56:25 -0300

    adicionado no ex_06_area_circulo.py da secao_01_estrutura_sequencial corretor de exercício.

--- secao_01_estrutura_sequencial/ex_06_area_circulo.py
+++ secao_01_estrutura_sequencial/ex_06_area_circulo.py
@@ -16,5 +16,30 @@ Observação: Use o valor de 3.1415 para o valor da constante π
 """
 
 
-def calcular_area_de_circulo():
+def calcular_area_de_circulo(area):
     """Escreva aqui em baixo a sua solução"""
+    PI = 3.1415
+    area = PI * (area ** 2)
+    return f"A área do círculo com esse raio é: {area:.4f}"
+
+def test(f, in_, expected):
+    """
+    Executa a função f com o parâmetro in_ e compara o resultado com expected.
+    :return: Exibe uma mensagem indicando se a função f está correta ou não.
+    """
+    out = f(in_)
+
+    if out == expected:
+        sign = '✅'
+        info = ''
+    else:
+        sign = '❌'
+        info = f'e o correto é {expected!r}'
+
+    print(f'{sign} {f.__name__}({in_!r}) retornou {out!r} {info}')
+
+
+if __name__ == '__main__':
+    # Testes que verificam o resultado do seu código em alguns cenários.
+    test(calcular_area_de_circulo, 1, "A área do círculo com esse raio é: 3.1415")
+    test(calcular_area_de_circulo, 2.5, "A área do círculo com esse raio é: 19.6344")
