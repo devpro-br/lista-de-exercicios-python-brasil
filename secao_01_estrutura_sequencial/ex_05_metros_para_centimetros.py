9ff3cc49fc9a553f1ae9929e2cf324df05a6197a
inczDan <daniel.alvmatos@gmail.com>
2022-05-25 15:35:44 -0300

    resolucao 5 lista 1

--- secao_01_estrutura_sequencial/ex_05_metros_para_centimetros.py
+++ secao_01_estrutura_sequencial/ex_05_metros_para_centimetros.py
@@ -21,6 +21,4 @@ def converter_metros_para_centimetros():
     metro=1_000
     # metro1= (float(input("Escreva quantos metros")))
     transformacao= metro/10
-    metro2= 3_621
-    transformando= metro2/10
     print("Transformando para centímetros dá", transformacao, "cm")
