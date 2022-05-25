80f496244a958c9bf77aeaebdaec25d4355b7326
renzo <renzo.nuccitelli@buser.com.br>
2022-05-25 14:53:15 -0300

    Feito gabarito da questão 10 da sessão 03 estrutura de repetição

--- secao_03_estrutura_de_repeticao/ex_10_gerar_numeros_de_intervalo.py
+++ secao_03_estrutura_de_repeticao/ex_10_gerar_numeros_de_intervalo.py
@@ -16,3 +16,4 @@ Faça um programa que receba dois números inteiros e gere os números inteiros 
 
 def calcular_numeros_no_intervalo(inicio: int, fim: int) -> str:
     """Escreva aqui em baixo a sua solução"""
+    return ', '.join(map(str, range(inicio, fim)))
