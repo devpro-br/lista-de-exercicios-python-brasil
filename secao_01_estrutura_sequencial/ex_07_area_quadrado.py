2aef148236785e8fdd86211634f3319604e8e465
DavidSilveira80 <davidpscontato@gmail.com>
2022-05-26 16:47:27 -0300

    Modificado o correetor de ex_07_area_quadrado.py da secao_01_estrutura_sequencial.

--- secao_01_estrutura_sequencial/ex_07_area_quadrado.py
+++ secao_01_estrutura_sequencial/ex_07_area_quadrado.py
@@ -5,18 +5,39 @@ https://wiki.python.org.br/EstruturaSequencial
 Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
 Mostrar a área com 2 casas decimais.
 
-    >>> from secao_01_estrutura_sequencial import ex_07_area_quadrado
-    >>> ex_07_area_quadrado.input = lambda k: '2'
-    >>> ex_07_area_quadrado.calcular_area_de_quadrado()
+    
+    lado 2
     A área do quadrado com esse lado é: 4.00
     O dobro da aŕea do quadrado é: 8.00
-    >>> ex_07_area_quadrado.input = lambda k: '2.5'
-    >>> ex_07_area_quadrado.calcular_area_de_quadrado()
+    lado 2.5
     A área do quadrado com esse lado é: 6.25
     O dobro da aŕea do quadrado é: 12.50
 
 """
 
 
-def calcular_area_de_quadrado():
+def calcular_area_de_quadrado(lado):
     """Escreva aqui em baixo a sua solução"""
+    
+
+def test(f, in_, expected):
+    """
+    Executa a função f com o parâmetro in_ e compara o resultado com expected.
+    :return: Exibe uma mensagem indicando se a função f está correta ou não.
+    """
+    out = f(in_)
+
+    if out == expected:
+        sign = '✅'
+        info = ''
+    else:
+        sign = '❌'
+        info = f'e o correto é {expected!r}'
+
+    print(f'{sign} {f.__name__}({in_!r}) retornou {out!r} {info}')
+
+
+if __name__ == '__main__':
+    # Testes que verificam o resultado do seu código em alguns cenários.
+    test(calcular_area_de_quadrado, 2, "A área do quadrado com esse lado é: 4.00\nO dobro da área do quadrado é: 8.00")
+    test(calcular_area_de_quadrado, 2.5, "A área do quadrado com esse lado é: 6.25\nO dobro da área do quadrado é: 12.50")
