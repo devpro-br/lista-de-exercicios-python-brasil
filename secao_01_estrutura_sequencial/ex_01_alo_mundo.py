a0f1c6b85a4c3711981ea43cc409c8e2a2293ce9
DavidSilveira80 <davidpscontato@gmail.com>
2022-05-25 21:24:22 -0300

    Modificado corretor automático.

--- secao_01_estrutura_sequencial/ex_01_alo_mundo.py
+++ secao_01_estrutura_sequencial/ex_01_alo_mundo.py
@@ -4,12 +4,30 @@ https://wiki.python.org.br/EstruturaSequencial
 
 Escreva um programa que mostre a mensagem "Olá Mundo"
 
-    >>> ola_mundo()
-    Olá Mundo
-
 """
 
-
-def ola_mundo():
+def ola_mundo(mensagem):
     """Escreva aqui em baixo a sua solução"""
-    print("Olá Mundo")
+    return f"{mensagem}"
+
+def test(f, in_, expected):
+    """
+    Executa a função f com o parâmetro in_ e compara o resultado com expected.
+    :return: Exibe uma mensagem indicando se a função f está correta ou não.
+    """
+    out = f(in_)
+
+    if out == expected:
+        sign = '✅'
+        info = ''
+    else:
+        sign = '❌'
+        info = f'e o correto é {expected!r}'
+
+    print(f'{sign} {f.__name__}({in_!r}) retornou {out!r} {info}')
+
+
+if __name__ == '__main__':
+    # Testes que verificam o resultado do seu código em alguns cenários.
+    test(ola_mundo, "Olá Mundo", "Olá Mundo")
+    
