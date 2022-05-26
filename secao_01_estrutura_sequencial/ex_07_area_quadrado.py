84f31406c86d6e02e820f9ac914034cd426351ee
Bruna Andrade <brunansandrade@gmail.com>
2022-05-26 16:28:28 -0300

    exercicio 07 s01 solucionado

--- secao_01_estrutura_sequencial/ex_07_area_quadrado.py
+++ secao_01_estrutura_sequencial/ex_07_area_quadrado.py
@@ -2,7 +2,8 @@
 Exercício 07 da seção de estrutura sequencial da Python Brasil:
 https://wiki.python.org.br/EstruturaSequencial
 
-Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+Faça um Programa que calcule a área de um quadrado, 
+em seguida mostre o dobro desta área para o usuário.
 Mostrar a área com 2 casas decimais.
 
     >>> from secao_01_estrutura_sequencial import ex_07_area_quadrado
@@ -20,3 +21,8 @@ Mostrar a área com 2 casas decimais.
 
 def calcular_area_de_quadrado():
     """Escreva aqui em baixo a sua solução"""
+    # A = b. h ou a=l**2
+    lado = float(input('Digite o lado de um quadrado: '))
+    area = lado ** 2
+    print(f'A área do quadrado com esse lado é: {area:.2f}')
+    print(f'O dobro da aŕea do quadrado é: {area*2:.2f}')
