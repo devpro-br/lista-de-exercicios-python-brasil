2deda475bfdcc003ed2a0f084701953ffb9edd56
Lucas Mariano <lucas.svmariano@gmail.com>
2022-05-25 22:39:21 -0300

    Seção 1 semifinalizada

--- secao_01_estrutura_sequencial/ex_10_celsius_para_fahrenheit.py
+++ secao_01_estrutura_sequencial/ex_10_celsius_para_fahrenheit.py
@@ -20,5 +20,5 @@ def transformar_para_fahrenheit():
     """Escreva aqui em baixo a sua solução"""
     c = float(input("Digite a temperatura em graus Celsius: "))
     f = (c * 1.8) + 32 
-    print("Essa temperatura é de",round(f),"Fahrenheit")
+    print(f"Essa temperatura é de",round(f),"Fahrenheit")
 
