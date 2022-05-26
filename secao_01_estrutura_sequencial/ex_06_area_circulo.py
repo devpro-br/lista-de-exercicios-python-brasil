75dc282738b5d8ff4b40b00e01b50c82914eb057
Bruna Andrade <brunansandrade@gmail.com>
2022-05-26 15:53:57 -0300

    exercicio 06 s01 resolvido

--- secao_01_estrutura_sequencial/ex_06_area_circulo.py
+++ secao_01_estrutura_sequencial/ex_06_area_circulo.py
@@ -18,3 +18,7 @@ Observação: Use o valor de 3.1415 para o valor da constante π
 
 def calcular_area_de_circulo():
     """Escreva aqui em baixo a sua solução"""
+    pi = 3.1415
+    raio = float(input('Digite o raio de um círculo para que sua área seja calculada: '))
+    area = pi * raio ** 2 
+    print(f'A área do círculo com esse raio é: {area:.4f}')
