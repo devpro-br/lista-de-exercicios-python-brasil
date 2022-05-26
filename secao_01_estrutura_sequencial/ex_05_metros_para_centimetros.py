a8990880e8b610c6f367fe5c882ab71cc233cf82
DavidSilveira80 <davidpscontato@gmail.com>
2022-05-25 23:02:45 -0300

    Modificado o corretor automático de ex_05_metros_para_centimetros.py da secao_01_estrutura_sequencial.

--- secao_01_estrutura_sequencial/ex_05_metros_para_centimetros.py
+++ secao_01_estrutura_sequencial/ex_05_metros_para_centimetros.py
@@ -4,16 +4,39 @@ https://wiki.python.org.br/EstruturaSequencial
 
 Faça um Programa que converta metros para centímetros.
 
-    >>> from secao_01_estrutura_sequencial import ex_05_metros_para_centimetros
-    >>> ex_05_metros_para_centimetros.input = lambda k: '1'
-    >>> ex_05_metros_para_centimetros.converter_metros_para_centimetros()
+    
+    Entrar com 1 metro
     Transformando para centímetros dá 100.0 cm
-    >>> ex_05_metros_para_centimetros.input = lambda k: '3.621'
-    >>> ex_05_metros_para_centimetros.converter_metros_para_centimetros()
+    Entrar com 3.621 metros
     Transformando para centímetros dá 362.1 cm
 
 """
 
 
-def converter_metros_para_centimetros():
+def converter_metros_para_centimetros(metro):
     """Escreva aqui em baixo a sua solução"""
+    
+    return f"Transformando para centímetros dá {float(metro) * 100} cm"
+
+
+def test(f, in_, expected):
+    """
+    Executa a função f com o parâmetro in_ e compara o resultado com expected.
+    :return: Exibe uma mensagem indicando se a função f está correta ou não.
+    """
+    out = f(in_)
+
+    if out == expected:
+        sign = '✅'
+        info = ''
+    else:
+        sign = '❌'
+        info = f'e o correto é {expected!r}'
+
+    print(f'{sign} {f.__name__}({in_!r}) retornou {out!r} {info}')
+
+
+if __name__ == '__main__':
+    # Testes que verificam o resultado do seu código em alguns cenários.
+    test(converter_metros_para_centimetros, 1, "Transformando para centímetros dá 100.0 cm")
+    test(converter_metros_para_centimetros, 3.621, "Transformando para centímetros dá 362.1 cm")
