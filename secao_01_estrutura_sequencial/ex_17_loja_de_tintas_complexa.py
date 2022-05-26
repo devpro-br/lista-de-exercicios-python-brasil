573871b0d10e72d462311c8354e80dde218a0a17
ronald_ps <ronald.seabra@buser.com.br>
2022-05-26 10:10:28 -0300

    enos o 18 feito. procurar diferença entre megabits e megabytes

--- secao_01_estrutura_sequencial/ex_17_loja_de_tintas_complexa.py
+++ secao_01_estrutura_sequencial/ex_17_loja_de_tintas_complexa.py
@@ -14,7 +14,7 @@ Acrescente 10% de folga e sempre arredonde os valores para cima, isto é, consid
     >>> from secao_01_estrutura_sequencial import ex_17_loja_de_tintas_complexa
     >>> ex_17_loja_de_tintas_complexa.input = lambda k: '100'
     >>> ex_17_loja_de_tintas_complexa.calcular_latas_e_preco_de_tinta()
-    Você deve comprar 18 litros de tinta.
+    Você deve comprar 19 litros de tinta.
     Você pode comprar 2 lata(s) de 18 litros a um custo de R$ 160. Vão sobrar 17.7 litro(s) de tinta.
     Você pode comprar 6 lata(s) de 3.6 litros a um custo de R$ 150. Vão sobrar 3.3 litro(s) de tinta.
     Para menor custo, você pode comprar 1 lata(s) de 18 litros e 1 galão(ões) de 3.6 litros a um custo de R$ 105. Vão sobrar 3.3 litro(s) de tinta.
@@ -26,10 +26,18 @@ Acrescente 10% de folga e sempre arredonde os valores para cima, isto é, consid
     Para menor custo, você pode comprar 2 lata(s) de 18 litros e 1 galão(ões) de 3.6 litros a um custo de R$ 185. Vão sobrar 2.9 litro(s) de tinta.
 
 """
+import math
 
 
 def calcular_latas_e_preco_de_tinta():
-
+    qunt_litros = (float(input('Digite os metros qadrados')) * 1.10) / 6
+    print(f'Você deve comprar {math.ceil(qunt_litros)} litros de tinta.')
+    print(f'Você pode comprar {math.ceil(qunt_litros / 18)} lata(s) de 18 litros a um custo de R$ {math.ceil(qunt_litros / 18) * 80}. Vão sobrar {((math.ceil(qunt_litros / 18) * 18) - qunt_litros): .1f} litro(s) de tinta.')
+    print(f'Você pode comprar {math.ceil(qunt_litros / 3.6)} lata(s) de 3.6 litros a um custo de R$ {math.ceil(qunt_litros / 3.6) * 25}. Vão sobrar {((math.ceil(qunt_litros / 3.6) * 3.6) - qunt_litros): .1f} litro(s) de tinta.')
+    if (qunt_litros - (qunt_litros//18)) >= 11.52 :
+        print(f'Para menor custo, você pode comprar {(qunt_litros // 18)+ 1} lata(s) de 18 litros e 0 galão(ões) de 3.6 litros a um custo de R$ {((qunt_litros // 18)+ 1) * 80}. Vão sobrar {((((qunt_litros // 18)+ 1) * 18) - qunt_litros): .1f} litro(s) de tinta.')
+    else:
+        print(f'Para menor custo, você pode comprar {qunt_litros//18} lata(s) de 18 litros e {math.ceil((qunt_litros%18)/ 3.6)} galão(ões) de 3.6 litros a um custo de R$ {(((qunt_litros//18)*80 + (math.ceil((qunt_litros%18)/ 3.6))* 25)):.1f}. Vão sobrar 2.9 litro(s) de tinta.')
 
 # Faça um Programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada.
 # Considere que a cobertura da tinta é de 1 litro para cada 6 metros quadrados e
