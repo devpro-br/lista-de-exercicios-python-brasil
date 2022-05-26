88c95e6ef3a0896560387d40c04a5fbf5b51bdeb
DavidSilveira80 <davidpscontato@gmail.com>
2022-05-25 22:44:40 -0300

    Modificado o corretor automático de ex_04_notas_bimestrais.py da secao_01_estrutura_sequencial.

--- secao_01_estrutura_sequencial/ex_04_notas_bimestrais.py
+++ secao_01_estrutura_sequencial/ex_04_notas_bimestrais.py
@@ -3,17 +3,33 @@ Exercício 03 da seção de estrutura sequencial da Python Brasil:
 https://wiki.python.org.br/EstruturaSequencial
 
 Faça um Programa que peça as 4 notas bimestrais e mostre a média.
-
-    >>> from secao_01_estrutura_sequencial import ex_04_notas_bimestrais
-    >>> numeros =['7', '8','9','10']
-    >>> ex_04_notas_bimestrais.input = lambda k: numeros.pop()
-    >>> ex_04_notas_bimestrais.calcular_media()
+    numeros =['7', '8','9','10']
     A média anual é 8.5
 
 """
 
-
-def calcular_media():
+def calcular_media(nota1, nota2, nota3, nota4):
     """Escreva aqui em baixo a sua solução"""
-    nota1, nota2, nota3, nota4 = 7, 8, 9, 10
-    print(f"A média anual é {(nota1 + nota2 + nota3 + nota4) / 4}")
+    return f"A média anual é {(nota1 + nota2 + nota3 + nota4) / 4}"
+
+def test(f, in_, expected):
+    """
+    Executa a função f com o parâmetro in_ e compara o resultado com expected.
+    :return: Exibe uma mensagem indicando se a função f está correta ou não.
+    """
+    out = f(*in_)
+
+    if out == expected:
+        sign = '✅'
+        info = ''
+    else:
+        sign = '❌'
+        info = f'e o correto é {expected!r}'
+
+    print(f'{sign} {f.__name__}({in_!r}) retornou {out!r} {info}')
+
+
+if __name__ == '__main__':
+    # Testes que verificam o resultado do seu código em alguns cenários.
+    test(calcular_media, (7, 8, 9, 10), "A média anual é 8.5")
+    
