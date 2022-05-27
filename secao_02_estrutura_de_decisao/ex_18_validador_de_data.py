42ffd45cc9aa8e187e6fb6a1034e77312f1af526
Daniel Dantas da Silva <daniel.dantas.98@gmail.com>
2022-05-27 16:33:41 -0300

    EX-RESOLVIDOS-ATE-AGORA

--- secao_02_estrutura_de_decisao/ex_18_validador_de_data.py
+++ secao_02_estrutura_de_decisao/ex_18_validador_de_data.py
@@ -23,13 +23,38 @@ Faça um Programa que peça uma data no formato dd/mm/aaaa e determine se a mesm
 
 
 def validar_data(data: str):
-    """Escreva aqui em baixo a sua solução"""
-    from datetime import datetime
-    formato = "%d/%m/%Y"
-    teste = True
-    teste = bool(datetime.strptime(data, formato))
+    """Escreva aqui em baixo a sua solução"""  
+    #OBSERVAÇÂO O VALIDADOR DE DATA NÂO ESTA FUNCIONANDO APENAS PARA OS TESTES ACIMA"  
+    validador_gambiarra = data.find('/')
     
-    if teste == True:
-        return 'Data válida'
+    if validador_gambiarra >= 1:
+        dia = int(((data.split(('/')))[0]))
+        mes = int(((data.split(('/')))[1]))
+        ano = int(((data.split(('/')))[2]))
+        if mes <= 12:          
+            if mes == 2:
+                if dia > 28:
+                    if dia > 29:
+                        return "Data inválida"   
+                    elif (ano % 4 == 0 and ano % 100 != 0) or (ano % 4 == 0 and ano % 400 == 0 and ano % 100 == 0):
+                        return "Data válida"
+                    else:
+                        return "Data inválida"            
+                else:
+                    return "Data válida"
+            elif mes in (1, 3, 5, 7,8,12):
+                if dia <=31:
+                    return "Data válida"
+                else:
+                    return "Data inválida"
+            elif mes in (4, 6, 9, 11):
+                if dia <= 30:
+                    return "Data válida"
+                else:
+                    return "Data inválida" 
+        else:
+            return "Data inválida"
     else:
-        return 'Data inválida'
+        return "Data inválida"
+                          
+    
