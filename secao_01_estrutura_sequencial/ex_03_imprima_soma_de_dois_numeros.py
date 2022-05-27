3e9a560cf2feb8cdce6ee76f77eebb9381599271
DavidSilveira80 <davidpscontato@gmail.com>
2022-05-26 22:40:39 -0300

    Solução de ex_03_imprima_soma_de_dois_numeros.py da secao_01_estrutura_sequencial.

--- secao_01_estrutura_sequencial/ex_03_imprima_soma_de_dois_numeros.py
+++ secao_01_estrutura_sequencial/ex_03_imprima_soma_de_dois_numeros.py
@@ -15,3 +15,4 @@ Faça um Programa que peça dois números inteiros e imprima a soma.
 
 def imprima_a_soma_de_dois_numeros():
     """Escreva aqui em baixo a sua solução"""
+    print(f'A soma dos dois números informados é {80 + 5}')
