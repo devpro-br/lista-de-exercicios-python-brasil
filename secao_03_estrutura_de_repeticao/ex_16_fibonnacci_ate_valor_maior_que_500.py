ec844cec86fa38b50a1b12b3ff937070fe0012f8
renzo <renzo.nuccitelli@buser.com.br>
2022-05-27 11:35:44 -0300

    Feita correção do exercício 16 da seção de repetição.
    close #33

---
+++ secao_03_estrutura_de_repeticao/ex_16_fibonnacci_ate_valor_maior_que_500.py
@@ -0,0 +1,14 @@
+"""
+Exercício 16 da seção de estrutura sequencial da Python Brasil:
+https://wiki.python.org.br/EstruturaDeRepeticao
+
+A série de Fibonacci é formada pela seqüência 0,1,1,2,3,5,8,13,21,34,55,... Faça um programa que gere a série até que o
+ valor seja maior que 500.
+
+    >>> calcular_serie_de_fibonacci_ate_valor_ser_maior_que_500()
+    '0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610'
+"""
+
+
+def calcular_serie_de_fibonacci_ate_valor_ser_maior_que_500() -> str:
+    """Escreva aqui em baixo a sua solução"""
