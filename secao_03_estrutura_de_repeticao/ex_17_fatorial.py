5b8cecc2ccd3c58a5f790e9327693353744a80ee
Renzo <renzon@gmail.com>
2022-05-30 18:11:05 -0300

    Feita solução para exercício 17 da seção de estruturas de repetição

--- secao_03_estrutura_de_repeticao/ex_17_fatorial.py
+++ secao_03_estrutura_de_repeticao/ex_17_fatorial.py
@@ -23,3 +23,7 @@ Ex.: 5!=5.4.3.2.1=120
 
 def calcular_fatorial(n: int) -> int:
     """Escreva aqui em baixo a sua solução"""
+    fatorial = 1
+    for n in range(2, n + 1):
+        fatorial *= n
+    return fatorial
