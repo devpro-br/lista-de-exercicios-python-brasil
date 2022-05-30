1fb9d2b7558e6002306e09eba291775b3dd20a7c
DavidSilveira80 <davidpscontato@gmail.com>
2022-05-30 15:31:45 -0300

    Ajuste nos inputs da linha 18 a 21 e no print de saída.

--- secao_01_estrutura_sequencial/ex_04_notas_bimestrais.py
+++ secao_01_estrutura_sequencial/ex_04_notas_bimestrais.py
@@ -15,7 +15,10 @@ Faça um Programa que peça as 4 notas bimestrais e mostre a média.
 
 def calcular_media():
     """Escreva aqui em baixo a sua solução"""
-    media = (7 + 8 + 9 + 10) / 4
-    print(f"A média anual é {media}")
+    nota1 = int(input("Digite a primeira nota: "))
+    nota2 = int(input("Digite a segunda nota: "))
+    nota3 = int(input("Digite a terceira nota: "))
+    nota4 = int(input("Digite a quarta nota: "))
     
-    # ou print(f"A média anual é {(7 + 8 + 9 + 10) / 4}")
+    print(f"A média anual é {(nota1 + nota2 + nota3 + nota4) / 4}")
+    
