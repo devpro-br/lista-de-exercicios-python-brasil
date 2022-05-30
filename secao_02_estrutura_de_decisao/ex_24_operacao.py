f81857400c05b5a8267cead674b3c3b2f56f8bf2
Daniel Dantas da Silva <daniel.dantas.98@gmail.com>
2022-05-30 15:44:04 -0300

    Ex.24 feito

--- secao_02_estrutura_de_decisao/ex_24_operacao.py
+++ secao_02_estrutura_de_decisao/ex_24_operacao.py
@@ -31,3 +31,41 @@ Mostre o restultado com duas casas decimais
 
 def fazer_operacao_e_classificar(n_1: float, n_2: float, operacao: str):
     """Escreva aqui em baixo a sua solução"""
+    #Realiza operação desejada
+    if operacao == '+':
+      resultado_operacao = n_1 + n_2
+    elif operacao == '-':
+      resultado_operacao = n_1 - n_2
+    elif operacao == '/':
+      resultado_operacao = n_1 / n_2
+    elif operacao == '*':
+      resultado_operacao = n_1 * n_2
+
+    #Testa se é decimal ou inteiro
+    if int(str(float(resultado_operacao)).split(('.'))[1]) >= 1:
+        decimal_inteiro = 'decimal.'
+    else:
+        decimal_inteiro = 'inteiro.'
+
+    #Testa se é positivo, negativo ou neutro
+    if resultado_operacao > 0:
+      positivo_negativo = 'positivo'
+    elif resultado_operacao == 0:
+      positivo_negativo = 'neutro'
+    else:
+      positivo_negativo = 'negativo'
+
+    #Testa se é par ou impar
+    teste = resultado_operacao % 2
+    if teste > 0:
+        par_impar = 'impar,'
+    else:
+        par_impar = 'par,'
+
+    #Entrega resposta
+    if decimal_inteiro == 'decimal.':
+      print('Resultado: %.2f' % resultado_operacao)
+      print('Número é', positivo_negativo, 'e', decimal_inteiro)
+    else:
+      print('Resultado: %.2f' % resultado_operacao)
+      print('Número é', par_impar, positivo_negativo, 'e', decimal_inteiro)       
