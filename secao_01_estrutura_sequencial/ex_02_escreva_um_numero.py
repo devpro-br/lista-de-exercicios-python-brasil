001fdddeaf4bf64ca927fa2c1c45dafc937f1e07
DavidSilveira80 <davidpscontato@gmail.com>
2022-05-30 15:21:15 -0300

    Ajuste no input na linha 16.

--- secao_01_estrutura_sequencial/ex_02_escreva_um_numero.py
+++ secao_01_estrutura_sequencial/ex_02_escreva_um_numero.py
@@ -13,5 +13,5 @@ Faça um Programa que peça um número e então mostre a mensagem O número info
 
 def escreva_um_numero():
     """Escreva aqui em baixo a sua solução"""
-    numero = 42
+    numero = int(input("Entre com qualquer número inteiro: "))
     print(f'O número informado foi {numero}')
