ac953f88ac87cbe82bd2df02516ca9627ff25d47
DavidSilveira80 <davidpscontato@gmail.com>
2022-05-30 15:57:17 -0300

    Solução de ex_01_imprimir_maior_numero.py da secao_02_estrutura_de_decisao.

--- secao_02_estrutura_de_decisao/ex_01_imprimir_maior_numero.py
+++ secao_02_estrutura_de_decisao/ex_01_imprimir_maior_numero.py
@@ -17,3 +17,9 @@ Faça um Programa que peça dois números e imprima o maior deles.
 
 def maior_de_dois_numeros(x, y):
     """Escreva aqui em baixo a sua solução"""
+    if x > y :
+        maior_numero = x
+    else:
+        maior_numero = y
+
+    print(maior_numero)
