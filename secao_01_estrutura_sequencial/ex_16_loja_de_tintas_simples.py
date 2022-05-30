c21a6a70c74944dad6a4dbc180df86c2e6cfd6ba
Bruna Andrade <brunansandrade@gmail.com>
2022-05-30 11:11:37 -0300

    ex 16 01

--- secao_01_estrutura_sequencial/ex_16_loja_de_tintas_simples.py
+++ secao_01_estrutura_sequencial/ex_16_loja_de_tintas_simples.py
@@ -18,6 +18,7 @@ Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e 
 
 """
 
+import math
 
 def calcular_latas_e_preco_de_tinta():
     """Escreva aqui em baixo a sua solução"""
