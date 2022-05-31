4e2c7dd1a03b02e3588171a29a2462c51d893244
Marcelo Silva <45953439+mhenrique94@users.noreply.github.com>
2022-05-31 10:38:16 -0300

    Corrigido exercício 04 da secao 01

--- secao_01_estrutura_sequencial/ex_04_notas_bimestrais.py
+++ secao_01_estrutura_sequencial/ex_04_notas_bimestrais.py
@@ -1,7 +1,6 @@
 """
 Exercício 04 da seção de estrutura sequencial da Python Brasil:
 https://wiki.python.org.br/EstruturaSequencial
-
 Faça um Programa que peça as 4 notas bimestrais e mostre a média.
     >>> from secao_01_estrutura_sequencial import ex_04_notas_bimestrais
     >>> numeros =['7', '8','9','10']
@@ -14,7 +13,6 @@ Faça um Programa que peça as 4 notas bimestrais e mostre a média.
 def calcular_media():
 
     """Escreva aqui em baixo a sua solução"""
-    # print("Calculo de média por meio de 4 notas.\n")
     num1 = int(input("Digite a primeira nota: "))
     num2 = int(input("Digite a segunda nota: "))
     num3 = int(input("Digite a terceira nota: "))
