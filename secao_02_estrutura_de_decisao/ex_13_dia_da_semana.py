1869e12ea1739459cc19510b72eacf275ea3475f
Marcelo Silva <45953439+mhenrique94@users.noreply.github.com>
2022-06-01 15:10:37 -0300

    Resolvido exercício 13 da seção 02

--- secao_02_estrutura_de_decisao/ex_13_dia_da_semana.py
+++ secao_02_estrutura_de_decisao/ex_13_dia_da_semana.py
@@ -29,3 +29,10 @@ se digitar outro valor deve aparecer valor inválido.
 
 def calcular_dia_da_semana(numero: int):
     """Escreva aqui em baixo a sua solução"""
+    dias = {1 : "'Domingo'",2 : "'Segunda'",3 : "'Terça'",4 : "'Quarta'",5 : "'Quinta'",6 : "'Sexta'",7 : "'Sábado'", }
+
+    if numero > 7 or numero <1:
+        print("'Dia Inválido'")
+    else:
+        print(dias[numero])
+calcular_dia_da_semana(1)
