e4d57ce1d2c1fd6e1079a5a68563fd41deb38602
Roberto Moraes <robertomoraeszar@gmail.com>
2022-06-02 11:20:57 -0300

    secao2 exs 1 a 7

--- secao_02_estrutura_de_decisao/ex_01_imprimir_maior_numero.py
+++ secao_02_estrutura_de_decisao/ex_01_imprimir_maior_numero.py
@@ -18,9 +18,7 @@ Faça um Programa que peça dois números e imprima o maior deles.
 def maior_de_dois_numeros(x, y):
     """Escreva aqui em baixo a sua solução"""
 
-    num1 = int(input("Digite um número"))
-    num2 = int(input("Digite outro número"))
-    if num1 > num2:
-        print(num1)
+    if x > y:
+        print(x)
     else:
-        print(num2)
+        print(y)
--- secao_02_estrutura_de_decisao/ex_06_maior_numero_de_3_numeros.py
+++ secao_02_estrutura_de_decisao/ex_06_maior_numero_de_3_numeros.py
@@ -17,3 +17,10 @@ Faça um Programa que leia três números e mostre o maior deles.
 
 def calcular_maior_de_3_numeros(x, y, z):
     """Escreva aqui em baixo a sua solução"""
+
+    if x > y and x > z:
+        print(x)
+    elif y > x and y > z:
+        print(y)
+    elif z > x and z > y:
+        print(z)
