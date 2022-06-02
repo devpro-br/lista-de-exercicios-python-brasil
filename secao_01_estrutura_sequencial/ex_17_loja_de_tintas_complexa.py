1abcdcf1a6af26c4fd42eac4319cc46461d205ad
Roberto Moraes <robertomoraeszar@gmail.com>
2022-06-02 09:39:34 -0300

    seção 1 ok

--- secao_01_estrutura_sequencial/ex_17_loja_de_tintas_complexa.py
+++ secao_01_estrutura_sequencial/ex_17_loja_de_tintas_complexa.py
@@ -21,7 +21,6 @@ Acrescente 10% de folga e sempre arredonde os valores para cima, isto é, consid
     >>> ex_17_loja_de_tintas_complexa.input = lambda k: '200'
     >>> ex_17_loja_de_tintas_complexa.calcular_latas_e_preco_de_tinta()
     Você deve comprar 37 litros de tinta.
-
     Você pode comprar 3 lata(s) de 18 litros a um custo de R$ 240. Vão sobrar 17.0 litro(s) de tinta.
     Você pode comprar 11 lata(s) de 3.6 litros a um custo de R$ 275. Vão sobrar 2.6 litro(s) de tinta.
     Para menor custo, você pode comprar 2 lata(s) de 18 litros e 1 galão(ões) de 3.6 litros a um custo de R$ 185. Vão sobrar 2.6 litro(s) de tinta.
@@ -42,18 +41,21 @@ def calcular_latas_e_preco_de_tinta():
 
     m2 = float(input('informe a quantidade de METRO QUADRADO (m²) a ser pintado: '))
 
-    consumo_litro = m2 / M2_POR_LITRO * MARGEM_SEGURANCA
-
+    consumo_litro = ceil(m2 / M2_POR_LITRO * MARGEM_SEGURANCA)
     qtd_galao_apenas = ceil(consumo_litro / LITROS_GALAO)
     valor_galao_apenas = qtd_galao_apenas * PRECO_GALAO
     qtd_lata_apenas = ceil(consumo_litro / LITROS_LATA)
-    valor_lata_apenas = qtd_lata_apenas * PRECO_LATA
+    valor_lata_apenas = ceil(qtd_lata_apenas * PRECO_LATA)
     qtd_galao_misto = ceil(consumo_litro // LITROS_GALAO)
     qtd_lata_misto = ceil((consumo_litro - qtd_galao_misto * LITROS_GALAO) / LITROS_LATA)
+    sobra_36 = (qtd_lata_apenas * 3.6) - consumo_litro
+    sobra_18 = (qtd_galao_apenas * 18) - consumo_litro
+    sobra_final = (qtd_galao_misto * 18) + (qtd_lata_misto * 3.6) - consumo_litro
     valor_galao_misto = qtd_galao_misto * PRECO_GALAO
     valor_lata_misto = qtd_lata_misto * PRECO_LATA
+    valor_misto_final = (qtd_galao_misto * 80) + (qtd_lata_misto * 25)
 
-    print(f'Você deve comprar {consumo_litro:.5f} litros de tinta')
-    print(f'Você pode comprar {qtd_galao_apenas:.0f} lata(s) de 18 litros a um custo de R$ {valor_galao_apenas:.2f}. Vão sobrar {qtd_galao_misto:.1f} litro(s) de tinta.')
-    print(f'Você pode comprar {qtd_lata_apenas:.0f} lata(s) de 3.6 litros a um custo de R$ {valor_lata_apenas:.2f}. Vão sobrar {qtd_lata_misto:.1f} litro(s) de tinta.')
-    print(f'Para menor custo, você pode comprar 2 lata(s) de 18 litros e 1 galão(ões) de 3.6 litros a um custo de R$ 185. Vão sobrar 2.6 litro(s) de tinta.')
+    print(f'Você deve comprar {consumo_litro:.0f} litros de tinta.')
+    print(f'Você pode comprar {qtd_galao_apenas:.0f} lata(s) de 18 litros a um custo de R$ {valor_galao_apenas:.0f}. Vão sobrar {sobra_18:.1f} litro(s) de tinta.')
+    print(f'Você pode comprar {qtd_lata_apenas:.0f} lata(s) de 3.6 litros a um custo de R$ {valor_lata_apenas:.0f}. Vão sobrar {sobra_36:.1f} litro(s) de tinta.')
+    print(f'Para menor custo, você pode comprar {qtd_galao_misto:.0f} lata(s) de 18 litros e {qtd_lata_misto:.0f} galão(ões) de 3.6 litros a um custo de R$ {valor_misto_final:.0f}. Vão sobrar {sobra_final:.1f} litro(s) de tinta.')
--- secao_02_estrutura_de_decisao/ex_01_imprimir_maior_numero.py
+++ secao_02_estrutura_de_decisao/ex_01_imprimir_maior_numero.py
@@ -17,3 +17,10 @@ Faça um Programa que peça dois números e imprima o maior deles.
 
 def maior_de_dois_numeros(x, y):
     """Escreva aqui em baixo a sua solução"""
+
+    num1 = int(input("Digite um número"))
+    num2 = int(input("Digite outro número"))
+    if num1 > num2:
+        print(num1)
+    else:
+        print(num2)
