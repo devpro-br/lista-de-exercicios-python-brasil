03c055c74dc5a3a57355d48bfffebca2840a7688
Marcelo Silva <45953439+mhenrique94@users.noreply.github.com>
2022-06-02 21:18:33 -0300

    Resolvido exercício 16

--- secao_02_estrutura_de_decisao/ex_16_equacao_segundo_grau.py
+++ secao_02_estrutura_de_decisao/ex_16_equacao_segundo_grau.py
@@ -26,3 +26,17 @@ Mostrar raízes com uma casa decimal.
 
 def resolver_equacao_do_segundo_grau(a: float, b: float, c: float):
     """Escreva aqui em baixo a sua solução"""
+      
+    delta = (b**2 - (4*a*c))
+    if a == 0:
+        print("'Valor do coeficiente a deve ser diferente de 0'")
+    
+    elif delta < 0:
+        print(f"'Delta negativo ({delta}), por isso não existem raízes reais'")
+    elif delta == 0:
+        x1 = (-b + delta**(1/2)) / (2*a)
+        print(f"'Delta é 0, raíz única no valor de {x1}'")
+    else:
+        x1 = (-b + delta**(1/2)) / (2*a)
+        x2 = (-b - delta**(1/2)) / (2*a)
+        print(f"'Delta é {delta}, raízes são {x1} e {x2}'")
