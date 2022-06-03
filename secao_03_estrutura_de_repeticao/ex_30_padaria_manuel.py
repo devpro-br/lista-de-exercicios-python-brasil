1cc5d2efe16ddd836dabf2fad2681b5f53048150
renzo <renzo.nuccitelli@buser.com.br>
2022-06-03 10:43:27 -0300

    Feito gabarito da questão 30 da seção de repetição.

--- secao_03_estrutura_de_repeticao/ex_30_padaria_manuel.py
+++ secao_03_estrutura_de_repeticao/ex_30_padaria_manuel.py
@@ -119,3 +119,8 @@ até 50 pães, a partir do preço do pão informado pelo usuário
 
 def gerar_tabela_de_precos(preco_por_pao: float):
     """Escreva aqui em baixo a sua solução"""
+    print(f'Preço do pão: R$ {preco_por_pao:5.2f}')
+    print('Panificadora Pão de Ontem - Tabela de preços')
+    for i in range(1, 51):
+        preco = preco_por_pao * i
+        print(f'{i:<2d} - R$ {preco:6.2f}')
