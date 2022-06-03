b8c9aebedea4a9d9b4815725d324917b0a3eda38
Marcelo Silva <45953439+mhenrique94@users.noreply.github.com>
2022-06-02 21:48:29 -0300

    Resolvido exercício 17

--- secao_02_estrutura_de_decisao/ex_17_ano_bissexto.py
+++ secao_02_estrutura_de_decisao/ex_17_ano_bissexto.py
@@ -20,3 +20,13 @@ bissexto.
 
 def eh_ano_bissexto(ano: int):
     """Escreva aqui em baixo a sua solução"""
+    if ano % 4 == 0:
+        if ano % 100 == 0:
+            if ano % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
