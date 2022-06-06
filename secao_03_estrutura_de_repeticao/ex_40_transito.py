90739117383d3b37c59f79d015cd557f356ccd37
renzo <renzo.nuccitelli@buser.com.br>
2022-06-06 15:18:45 -0300

    Corrigido arquivo do ex 40 da seção de estrutura de repetição

--- secao_03_estrutura_de_repeticao/ex_40_transito.py
+++ secao_03_estrutura_de_repeticao/ex_40_transito.py
@@ -19,36 +19,12 @@ Mostre os valores com uma casa decimail
     >>> calcular_estatisticas(('SJC', 190_000, 300), ('SP', 1_000_000, 2_000 ),
     ... ('BH', 800_000, 1000), ('FZ', 600_000, 700), ('FL', 150_000, 900)
     ... )
-    O maior índice de acidentes é de FZ, com
-    O menor índice de acidentes é de , com
-    O média de veículos por cidade é de
-    A média de acidentes total nas cidades com menos de 150 mil habiltante é de
-
+    O maior índice de acidentes é de FL, com 6.0 acidentes por mil habitantes.
+    O menor índice de acidentes é de FZ, com 1.2 acidentes por mil habitantes.
+    O média de veículos por cidade é de 548000.
+    A média de acidentes total nas cidades com menos de 150 mil habitantes é de 900.0 acidentes.
 """
-from statistics import mean
 
 
 def calcular_estatisticas(*cidades):
     """Escreva aqui em baixo a sua solução"""
-
-    def calcular_indice_de_acidentes(cidade):
-        return cidade[2] * 1000 / cidade[1]
-
-    cidade_maior_indice = max(cidades, key=calcular_indice_de_acidentes)
-    nome_maior_indice = cidade_maior_indice[0]
-    maior_indice = calcular_indice_de_acidentes(cidade_maior_indice)
-
-    cidade_menor_indice = min(cidades, key=calcular_indice_de_acidentes)
-    nome_menor_indice = cidade_menor_indice[0]
-    menor_indice = calcular_indice_de_acidentes(cidade_menor_indice)
-
-    media_veiculos_por_cidade = mean(cidade[1] for cidade in cidades)
-    media_veiculos_por_cidade_pequenas = mean(cidade[2] for cidade in cidades if cidade[1] <= 150_000)
-
-    print(f'O maior índice de acidentes é de {nome_maior_indice}, com {maior_indice:.1f} acidentes por mil habitantes.')
-    print(f'O menor índice de acidentes é de {nome_menor_indice}, com {menor_indice:.1f} acidentes por mil habitantes.')
-    print(f'O média de veículos por cidade é de {media_veiculos_por_cidade}.')
-    print(
-        f'A média de acidentes total nas cidades com menos de 150 mil habitantes é de '
-        f'{media_veiculos_por_cidade_pequenas:.1f} acidentes.'
-    )
