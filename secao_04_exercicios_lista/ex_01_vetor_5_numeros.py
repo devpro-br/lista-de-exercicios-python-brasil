5c96f7c488a2e88a1d55a52211ae2f45e6649c0e
renzo <renzo.nuccitelli@buser.com.br>
2022-06-07 10:12:22 -0300

    Criada infraestrura de testes para seções 4, 5 e 6

---
+++ secao_04_exercicios_lista/ex_01_vetor_5_numeros.py
@@ -0,0 +1,14 @@
+"""
+Exercício 01 da seção de listas da Python Brasil:
+https://wiki.python.org.br/ExerciciosListas
+
+Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
+
+    >>> ler_5_valores()
+
+"""
+
+
+def ler_5_valores():
+    """Escreva aqui em baixo a sua solução"""
+
---
+++ secao_05_exercicios_funcoes/ex_01_piramide.py
@@ -0,0 +1,15 @@
+"""
+Exercício 01 da seção de listas da Python Brasil:
+https://wiki.python.org.br/ExerciciosFuncoes
+
+Faça um programa para imprimir:
+
+  1
+  2   2
+  3   3   3
+  .....
+  n   n   n   n   n   n  ... n
+
+para um n informado pelo usuário. Use uma função que receba um valor n inteiro e imprima até a n-ésima linha.
+
+"""
