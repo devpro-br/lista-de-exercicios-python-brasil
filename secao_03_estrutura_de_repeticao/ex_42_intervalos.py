26571cd5d3231e913b66881bfb989392ad4496b1
renzo <renzo.nuccitelli@buser.com.br>
2022-06-07 09:28:59 -0300

    Corrido problema no execício 42 da seção de estrutura de repetição

--- secao_03_estrutura_de_repeticao/ex_42_intervalos.py
+++ secao_03_estrutura_de_repeticao/ex_42_intervalos.py
@@ -11,7 +11,7 @@ A entrada de dados deverá terminar quando for lido um número negativo.
     >>> ex_42_intervalos.input = lambda k: numeros_para_avaliacao.pop()
     >>> ex_42_intervalos.listar_numeros_para_avaliacao()
     7 número(s) entre o intervalo de zero a 25
-    1 número(s) entre o intervalo de 25 a 50
+    1 número(s) entre o intervalo de 26 a 50
     1 número(s) entre o intervalo de 76 a 100
     >>> from secao_03_estrutura_de_repeticao import  ex_42_intervalos
     >>> numeros_para_avaliacao=[14, -5, 10, 2, 80, 99]
@@ -20,18 +20,18 @@ A entrada de dados deverá terminar quando for lido um número negativo.
     2 número(s) entre o intervalo de zero a 25
     2 número(s) entre o intervalo de 76 a 100
     >>> from secao_03_estrutura_de_repeticao import  ex_42_intervalos
-    >>> numeros_para_avaliacao=[144, 5, 32, 18, 43, 12, 26, 76]
+    >>> numeros_para_avaliacao=[-55, 144, 5, 32, 18, 43, 12, 26, 76]
     >>> ex_42_intervalos.input = lambda k: numeros_para_avaliacao.pop()
     >>> ex_42_intervalos.listar_numeros_para_avaliacao()
     3 número(s) entre o intervalo de zero a 25
-    3 número(s) entre o intervalo de 25 a 50
+    3 número(s) entre o intervalo de 26 a 50
     1 número(s) entre o intervalo de 76 a 100
     >>> from secao_03_estrutura_de_repeticao import  ex_42_intervalos
     >>> numeros_para_avaliacao=[3, 5, 100, -5, 70, 88, 28, 12]
     >>> ex_42_intervalos.input = lambda k: numeros_para_avaliacao.pop()
     >>> ex_42_intervalos.listar_numeros_para_avaliacao()
     1 número(s) entre o intervalo de zero a 25
-    1 número(s) entre o intervalo de 25 a 50
+    1 número(s) entre o intervalo de 26 a 50
     1 número(s) entre o intervalo de 51 a 75
     1 número(s) entre o intervalo de 76 a 100
 
