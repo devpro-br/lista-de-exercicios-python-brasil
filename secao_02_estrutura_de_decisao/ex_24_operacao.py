bada595d1140d10221438053c8a83732cb9edf76
gislane.nunes <gislane_nunes@yahoo.com.br>
2022-06-07 11:29:49 -0300

    organizando o código seção 2

--- secao_02_estrutura_de_decisao/ex_24_operacao.py
+++ secao_02_estrutura_de_decisao/ex_24_operacao.py
@@ -50,7 +50,7 @@ def fazer_operacao_e_classificar(n_1: float, n_2: float, operacao: str):
     else:
       print(f'Números inválidos.')
       
-    if resultado%2 == 0:
+    if resultado % 2 == 0:
         par_ou_impar = 'par'
     else:
         par_ou_impar = 'impar'
