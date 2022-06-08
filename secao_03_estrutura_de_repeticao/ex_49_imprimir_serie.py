fd2c0e49729a929d9c3d05310fc54c1cc772139f
Renzo <renzon@gmail.com>
2022-06-08 13:44:07 -0300

    removido import desnecessários nas questões 49 e 50 de estrutura de repetição

--- secao_03_estrutura_de_repeticao/ex_49_imprimir_serie.py
+++ secao_03_estrutura_de_repeticao/ex_49_imprimir_serie.py
@@ -19,17 +19,16 @@ Faça um programa que mostre os n termos da Série a seguir:
     ----------------------------------
     
 
-    >>> from secao_03_estrutura_de_repeticao import  ex_49_imprimir_serie
-    >>> ex_49_imprimir_serie.imprimir_serie(5)
+    >>> imprimir_serie(5)
     S = 1/1 + 2/3 + 3/5 + 4/7 + 5/9
     soma = 3.393650793650793
-    >>> ex_49_imprimir_serie.imprimir_serie(7)
+    >>> imprimir_serie(7)
     S = 1/1 + 2/3 + 3/5 + 4/7 + 5/9 + 6/11 + 7/13
     soma = 4.477566877566877
-    >>> ex_49_imprimir_serie.imprimir_serie(3)
+    >>> imprimir_serie(3)
     S = 1/1 + 2/3 + 3/5
     soma = 2.2666666666666666
-    >>> ex_49_imprimir_serie.imprimir_serie(9)
+    >>> imprimir_serie(9)
     S = 1/1 + 2/3 + 3/5 + 4/7 + 5/9 + 6/11 + 7/13 + 8/15 + 9/17
     soma = 5.540311975606093
 
