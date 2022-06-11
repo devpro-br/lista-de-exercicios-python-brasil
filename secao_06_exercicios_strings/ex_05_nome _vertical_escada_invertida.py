396d9d39be94354ab9f89e7020b318aa9bbff9c1
Victor Magalhaes <victor.barreto@buser.com.br>
2022-06-11 20:33:09 -0300

    Feito gabarito da questão 05 da seção de strings.
    close #70

---
+++ secao_06_exercicios_strings/ex_05_nome _vertical_escada_invertida.py
@@ -0,0 +1,36 @@
+"""
+Exercício 05 da seção de strings da Python Brasil:
+https://wiki.python.org.br/ExerciciosComStrings
+
+Nome na vertical em escada invertida. Altere o programa anterior de modo que a escada seja invertida.
+
+    >>> inverter_escada('CAMARGUINHO')
+    CAMARGUINHO
+    CAMARGUINH
+    CAMARGUIN
+    CAMARGUI
+    CAMARGU
+    CAMARG
+    CAMAR
+    CAMA
+    CAM
+    CA
+    C
+    >>> inverter_escada('ENZO PASCOAL')
+    ENZO PASCOAL
+    ENZO PASCOA
+    ENZO PASCO
+    ENZO PASC
+    ENZO PAS
+    ENZO PA
+    ENZO P
+    ENZO
+    ENZ
+    EN
+    E
+
+"""
+
+
+def inverter_escada(nome:str):
+    """Escreva aqui em baixo a sua solução"""
