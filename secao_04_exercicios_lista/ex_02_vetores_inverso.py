05c77585c117261c48040a2cf8f1b9d1ceac5939
marilia-borgo <marilia.bm01@gmail.com>
2022-06-14 16:21:06 -0300

    issue #61 sessao 4 exercicio 2

--- secao_04_exercicios_lista/ex_02_vetores_inverso.py
+++ secao_04_exercicios_lista/ex_02_vetores_inverso.py
@@ -17,5 +17,4 @@ Faça um Programa que leia um vetor de 10 números reais e mostre-os na ordem in
 
 def inverter_vetores(inteiros: list) -> str:
     """Escreva aqui em baixo a sua solução"""
-    inteiros.reverse()
-    print(inteiros)
+    
