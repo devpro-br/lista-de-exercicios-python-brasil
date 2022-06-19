5561ab82e41b4cb53cc26c9fabd3700c07883657
Fabio Freitas <fabiofsrj@gmail.com>
2022-06-19 19:58:14 -0300

    ajuste no codigo eercicio 19 est. decisao da indentação com a função do doctest

--- secao_02_estrutura_de_decisao/ex_19_decomposicao_de_numero.py
+++ secao_02_estrutura_de_decisao/ex_19_decomposicao_de_numero.py
@@ -52,62 +52,62 @@ Observando os termos no plural a colocação do "e", da vírgula entre outros. E
 
 def decompor_numero(numero: int):
     
-while True:
-    try:
-        
-        numero = int(input('Digite um número positivo menor que 1000: '))
-        
-        if numero >= 1000:
-            print('O número precisa ser menor que 1000')
-        elif numero < 0:
-            print('O número precisa ser positivo')
-        else:
+    while True:
+        try:
             
-            cent_int = dez_int = und_int = 0
-            cent_str = dez_str = und_str = ''
-            resto = 0
-           
+            numero = int(input('Digite um número positivo menor que 1000: '))
             
-            cent_int, resto = divmod(numero,100)
-            if cent_int != 0:
-                cent_str = " centena"
-                if cent_int > 1:
-                    cent_str += "s"
-            
-            dez_int, resto = divmod(numero - (cent_int * 100),10)
-            if dez_int != 0:
-                dez_str = " dezena"
-                if dez_int > 1:
-                    dez_str += "s"
-            
-            und_int = resto 
-            if und_int != 0:
-                und_str = " unidade"
-                if und_int > 1:
-                    und_str += "s"
-            
-            # 101 ou 100
-            if cent_int != 0 and dez_int == 0:
-                mensagem = str(cent_int) + cent_str
+            if numero >= 1000:
+                print('O número precisa ser menor que 1000')
+            elif numero < 0:
+                print('O número precisa ser positivo')
+            else:
+                
+                cent_int = dez_int = und_int = 0
+                cent_str = dez_str = und_str = ''
+                resto = 0
+               
+                
+                cent_int, resto = divmod(numero,100)
+                if cent_int != 0:
+                    cent_str = " centena"
+                    if cent_int > 1:
+                        cent_str += "s"
+                
+                dez_int, resto = divmod(numero - (cent_int * 100),10)
+                if dez_int != 0:
+                    dez_str = " dezena"
+                    if dez_int > 1:
+                        dez_str += "s"
+                
+                und_int = resto 
                 if und_int != 0:
-                    mensagem += " e " + str(und_int) + und_str
-           
-            # 110 
-            if cent_int != 0 and dez_int != 0 and und_int == 0:
-                mensagem = str(cent_int) + cent_str + " e " + str(dez_int) +dez_str
-            # 111   
-            elif cent_int != 0 and dez_int != 0 and und_int != 0:
-                mensagem = str(cent_int) + cent_str + ", " + str(dez_int) + dez_str + ' e ' + str(und_int) + und_str
+                    und_str = " unidade"
+                    if und_int > 1:
+                        und_str += "s"
+                
+                # 101 ou 100
+                if cent_int != 0 and dez_int == 0:
+                    mensagem = str(cent_int) + cent_str
+                    if und_int != 0:
+                        mensagem += " e " + str(und_int) + und_str
+               
+                # 110 
+                if cent_int != 0 and dez_int != 0 and und_int == 0:
+                    mensagem = str(cent_int) + cent_str + " e " + str(dez_int) +dez_str
+                # 111   
+                elif cent_int != 0 and dez_int != 0 and und_int != 0:
+                    mensagem = str(cent_int) + cent_str + ", " + str(dez_int) + dez_str + ' e ' + str(und_int) + und_str
+                
+                # 011
+                elif cent_int == 0 and dez_int != 0 and und_int != 0:
+                    mensagem = str(dez_int) + dez_str + ' e ' + str(und_int) + und_str
+                # 010
+                elif cent_int == 0 and dez_int != 0 and und_int == 0:
+                     mensagem = str(dez_int) + dez_str
+                
             
-            # 011
-            elif cent_int == 0 and dez_int != 0 and und_int != 0:
-                mensagem = str(dez_int) + dez_str + ' e ' + str(und_int) + und_str
-            # 010
-            elif cent_int == 0 and dez_int != 0 and und_int == 0:
-                 mensagem = str(dez_int) + dez_str
-            
-        
-            print(f'{numero} = {mensagem}')
-            break
-    except ValueError:
-        print('Entrada inválida')
+                print(f'{numero} = {mensagem}')
+                break
+        except ValueError:
+            print('Entrada inválida')
