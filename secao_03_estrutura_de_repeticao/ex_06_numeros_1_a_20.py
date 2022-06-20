b29fa5b087f989bd0b23c58711bf4dab3d3d04d7
Edinaildo Costa <edinaildopereira2013@gmail.com>
2022-06-19 23:38:59 -0300

    resolvi alguns da lista 3

--- secao_03_estrutura_de_repeticao/ex_06_numeros_1_a_20.py
+++ secao_03_estrutura_de_repeticao/ex_06_numeros_1_a_20.py
@@ -33,3 +33,16 @@ mostre os números um ao lado do outro.
 
 def escrever_numeros_de_1_a_20(formato:str) -> str:
     """Escreva aqui em baixo a sua solução"""
+    numeros = range(1, 21)
+
+    if formato == 'um_abaixo_do_outro':
+        for i in numeros:
+            print(i)
+
+    else:
+        if formato != 'um_abaixo_do_outro':
+            for i in numeros:
+                if i == numeros[-1]:
+                    print(f'{i}')
+                else:
+                    print(f'{i}', end=" ")
