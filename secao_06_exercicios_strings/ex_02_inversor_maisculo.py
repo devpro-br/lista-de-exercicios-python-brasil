5e79434f4c4c1db21feebe7bf83ac31172ade8eb
Lucca Nunes <luccanrib@gmail.com>
2022-06-20 22:56:39 -0300

    Feito a correção do exercícios 02 da lista de strings.
    close #80

---
+++ secao_06_exercicios_strings/ex_02_inversor_maisculo.py
@@ -0,0 +1,22 @@
+"""
+Exercício 02 da seção de listas da Python Brasil:
+https://wiki.python.org.br/ExerciciosComStrings
+
+Faça um programa que mostre o nome do usuário de trás para frente utilizando somente
+letras maiúsculas. Dica: lembre−se que ao informar o nome o usuário pode digitar
+letras maiúsculas ou minúsculas.
+
+
+    >>> inversor('lucca')
+    ACCUL
+    >>> inversor('douglas')
+    SALGUOD
+    >>> inversor('TaTIana')
+    ANAITAT
+    >>> inversor('MARIa')
+    AIRAM
+
+"""
+
+
+def inversor(nome):
