c0470c7209eab11922912f092187a922cceaf959
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2022-06-22 11:44:20 -0300

    criado o teste para o exercício 14

---
+++ secao_04_exercicios_lista/ex_14_investigacao.py
@@ -0,0 +1,35 @@
+"""
+Exercício 14 da seção de listas da Python Brasil:
+https://wiki.python.org.br/ExerciciosListas
+
+Faça um programa que faça 5 perguntas para uma pessoa sobre um crime. As perguntas são:
+
+  "Telefonou para a vítima?"
+  "Esteve no local do crime?"
+  "Mora perto da vítima?"
+  "Devia para a vítima?"
+  "Já trabalhou com a vítima?"
+
+O programa deve no final emitir uma classificação sobre a participação da pessoa no crime.
+Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeito",
+entre 3 e 4 como "Cúmplice" e 5 como "Assassino".
+Caso contrário, ele será classificado como "Inocente".
+
+    >>> investigar('Sim','Sim','Sim','Sim','Sim')
+    'Assassino'
+    >>> investigar('Sim','Sim','Sim','Sim','Não')
+    'Cúmplice'
+    >>> investigar('Sim','Sim','Sim','Não','Não')
+    'Cúmplice'
+    >>> investigar('Sim','Sim','Não','Não','Não')
+    'Suspeito'
+    >>> investigar('Sim','Não','Não','Não','Não')
+    'Inocente'
+    >>> investigar('Não','Não','Não','Não','Não')
+    'Inocente'
+
+"""
+
+
+def investigar(telefonou: str, estava_no_local: str, mora_perto: str, devia: str, trabalhou: str, ):
+    """Escreva aqui em baixo a sua solução"""
