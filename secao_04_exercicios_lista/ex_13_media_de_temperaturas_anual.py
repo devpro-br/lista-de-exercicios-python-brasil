c6cd3d518f0f55c3e29b24100ddcadd56f239b27
Ronald-ps <104438755+Ronald-ps@users.noreply.github.com>
2022-06-22 17:54:47 -0300

    exercicio 13 - secao 4 - ronald (#115)
    Feito teste de exercicio 13, seçãoo 04
    
    close #113

---
+++ secao_04_exercicios_lista/ex_13_media_de_temperaturas_anual.py
@@ -0,0 +1,45 @@
+"""
+Exercício 01 da seção de listas da Python Brasil:
+https://wiki.python.org.br/ExerciciosListas
+
+Faça um programa que receba a temperatura média de cada mês do ano e armazene-as em uma lista.
+Após isto, calcule e MOSTRE A MÉDIA ANUAL das temperaturas e MOSTRE TODAS AS TEMPERATURAS ACIMA DA MÉDIA ANUAL,
+e em que mês elas ocorreram (mostrar o mês por extenso: 1 – Janeiro, 2 – Fevereiro, . . . ).
+
+-as temperaturas só serão dadas em inteiro
+-todos os meses do ano serão passados à funçao, começando de janeiro e terminando em dezembro.
+ Todos seguidos de sua temperatura mensal
+
+(Funçoês recomendadas para estudo:
+    - .ljust()
+    - enumerate()
+    - sum()
+    - len()
+
+
+    >>> from secao_04_exercicios_lista import ex_13_media_de_temperaturas_anuais
+
+    >>> meses_vs_temperaturas = ['25','Dezembro', '33','Novembro','19','Outubro','16','Setembro','15','Agosto','20', 'Julho','25', 'Junho','29','Maio' ,'25', 'Abril','27','Março','33', 'Fevereiro', '30', 'Janeiro']
+    >>> ex_13_media_de_temperaturas_anuais.input = lambda k: meses_vs_temperaturas.pop()
+    >>> ex_13_media_de_temperaturas_anuais.temperaturas_acima_da_media()
+    Média anual: 24.75 Graus
+     1 - Janeiro:       30°
+     2 - Fevereiro:     33°
+     3 - Março:         27°
+     4 - Abril:         25°
+     5 - Maio:          29°
+     6 - Junho:         25°
+    11 - Novembro:      33°
+    12 - Dezembro:      25°
+
+
+
+
+
+
+"""
+
+def temperaturas_acima_da_media():
+    """Escreva aqui sua solução: """
+
+
