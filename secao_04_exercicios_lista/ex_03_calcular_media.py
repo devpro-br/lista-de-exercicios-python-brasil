073a52408bdcd4b895226466bcd9c3d336cd3a50
Cainalucas <cainam1@hotmail.com>
2022-06-22 20:41:23 -0300

    testeseccao4ex3

---
+++ secao_04_exercicios_lista/ex_03_calcular_media.py
@@ -0,0 +1,15 @@
+'''
+Faça um Programa que leia 4 notas, mostre as notas e a média na tela.
+
+    >>> from secao_04_exercicios_lista import  ex_03_calcular_media
+    >>> ex_03_calcular_media. calcular_media(4.2,5.1,6,7)
+    As notas são: 4.2, 5.1, 6, 7 e a média é: 5.58
+    >>> ex_03_calcular_media. calcular_media(9.2,7.1,9,8)
+    As notas são: 9.2, 7.1, 9, 8 e a média é: 8.32
+    >>> ex_03_calcular_media. calcular_media(9.2,8.1,7,9)
+    As notas são: 9.2, 8.1, 7, 9 e a média é: 8.32
+
+'''
+
+def calcular_media(*notas: float):
+    """Escreva aqui em baixo a sua solução"""
