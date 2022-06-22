655b6fb6ec725267dcd3f65917cb59a481946785
inczDan <daniel.alvmatos@gmail.com>
2022-06-22 14:54:49 -0300

    resolvido ex 09 secao strings

---
+++ secao_06_exercicios_strings/ex_09_validador_de_ cpf_strings.py
@@ -0,0 +1,29 @@
+"""
+Exercício 09 da seção de strings da Python Brasil:
+https://wiki.python.org.br/ExerciciosComStrings
+
+Desenvolva um programa que solicite a digitação de um número de CPF no formato xxx.xxx.xxx-xx
+e indique se é um número válido ou inválido através da validação dos dígitos verificadores
+e dos caracteres de formatação.
+
+    >>> validar_cpf('734.289.251-30')
+    True
+
+
+    >>> validar_cpf('732.289.294-10')
+    False
+
+
+    >>> validar_cpf('44986045040')
+    True
+
+
+    >>> validar_cpf('6693171008')
+    False
+
+
+"""
+
+
+def validar_cpf(cpf):
+    """Escreva aqui em baixo a sua solução"""
