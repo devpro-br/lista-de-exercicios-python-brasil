7f68243c8794893db33f32bee087a671d8914557
David Aguina <aguinadavid@gmail.com>
2022-06-22 00:08:25 -0300

    Feito teste do exercício 08 da lista de funções    close #74

---
+++ secao_05_exercicios_funcoes/ex_08_quantidade_digitos.py
@@ -0,0 +1,25 @@
+"""
+Exercício 08 da seção de funções da Python Brasil:
+https://wiki.python.org.br/ExerciciosFuncoes
+
+Faça uma função que informe a quantidade de dígitos de um determinado número inteiro informado.
+
+    >>> imprimir_qtde_digitos_do_inteiro(11235813213455)
+    14
+
+    >>> imprimir_qtde_digitos_do_inteiro(14916253649)
+    11
+
+    >>> imprimir_qtde_digitos_do_inteiro(-273)
+    3
+
+    >>> imprimir_qtde_digitos_do_inteiro(3.14159265359)
+    O valor informado não é um inteiro
+
+    >>> imprimir_qtde_digitos_do_inteiro(29011998)
+    8
+
+    >>> imprimir_qtde_digitos_do_inteiro(0)
+    1
+
+"""
