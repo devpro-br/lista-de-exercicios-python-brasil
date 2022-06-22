070ae1d7238101b449d0464d52e29930364f1706
Fabio Freitas <fabiofsrj@gmail.com>
2022-06-22 17:07:43 -0300

    resolução do exercicio 22 da lista estrutura de decisão

--- secao_02_estrutura_de_decisao/ex_22_par_ou_impar.py
+++ secao_02_estrutura_de_decisao/ex_22_par_ou_impar.py
@@ -21,4 +21,20 @@ Faça um Programa que peça um número inteiro e determine se ele é par ou impa
 
 
 def decidir_se_eh_par_ou_impar(valor: int) -> str:
-    """Escreva aqui em baixo a sua solução"""
+    while True:
+        try:
+
+            num = int(input('Digite o número a consultar se é par: '))
+
+            result = num % 2
+
+            if result == 0:
+                resposta = "Par"
+            else:
+                resposta = "Impar"
+
+            print(resposta)
+        
+        except ValueError:
+            print("Entrada inválida!!!")
+
