e411ceb075fbd4b78648d05122219915291bc28e
Edinaildo Costa <edinaildopereira2013@gmail.com>
2022-06-23 20:12:34 -0300

    feito o teste do ex 07 da lista de String #82

---
+++ secao_06_exercicios_strings/ex_07_conta _espacos _e _vogais.py
@@ -0,0 +1,23 @@
+"""
+Exercício 07 da seção de strings da Python Brasil:
+https://wiki.python.org.br/ExerciciosComStrings
+
+Dado a string abaixo (incluindo espaços em branco), conte:
+
+a) Quantos espaços em branco existem na frase.
+
+b) Quantas vezes aparecem as vogais a, e, i, o, u.
+
+    >>> contar_espacos_e_vogais("Enzo Pascal é o maior Buserteco")
+    A frase tem 11 vogais e 5 espaços em branco.
+    >>> contar_espacos_e_vogais("Somos todos Buserlovers")
+    A frase tem 8 vogais e 2 espaços em branco.
+"""
+
+
+def contar_espacos_e_vogais(nome):
+    """Escreva aqui em baixo a sua solução"""
+
+
+    
+
