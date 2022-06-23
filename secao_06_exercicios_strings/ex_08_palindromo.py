380db117a841eb31a1c6b56e547b3b6a8065291f
Beatriz Esteves <beatrizramalho.esteves@gmail.com>
2022-06-23 14:12:47 -0300

    “Soluçã0

---
+++ secao_06_exercicios_strings/ex_08_palindromo.py
@@ -0,0 +1,24 @@
+"""
+Exercício 01 da seção de listas da Python Brasil:
+https://wiki.python.org.br/ExerciciosComStrings
+
+Palíndromo. Um palíndromo é uma seqüência de caracteres cuja leitura é idêntica se feita da direita para esquerda ou vice−versa. 
+Por exemplo: OSSO e OVO são palíndromos. Em textos mais complexos os espaços e pontuação são ignorados. 
+A frase SUBI NO ONIBUS é o exemplo de uma frase palíndroma onde os espaços foram ignorados. Faça um programa que 
+leia uma seqüência de caracteres, mostre−a e diga se é um palíndromo ou não.
+
+	>>> palindromo('OSSO')
+	É palíndromo
+	>>> palindromo('SUBI NO ONIBUS')
+	É palíndromo
+	>>> palindromo('AME O POEMA')
+	É palíndromo
+	>>> palindromo('ROTAS')
+	Não é palíndromo
+	>>> palindromo('POEMA')
+	Não é palíndromo
+
+"""
+
+def palindromo(expressao:str):
+	"""Escreva aqui em baixo a sua solução"""
