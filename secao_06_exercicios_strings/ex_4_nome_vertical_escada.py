e1a6c1de4403e684a60bf1c030cfc7924afb2a47
Marcos Ornellas <129.ornellas@gmail.com>
2022-06-23 11:17:04 -0300

    Doctest Ex. 04 - sec. 06

---
+++ secao_06_exercicios_strings/ex_4_nome_vertical_escada.py
@@ -0,0 +1,50 @@
+"""
+Exercício 04 da seção de exercícios com strings da Python Brasil:
+https://wiki.python.org.br/ExerciciosComStrings
+
+Nome na vertical em escada. Modifique o programa anterior de forma a mostrar o nome em formato de escada.
+
+    >>> nome_escada('Arnold_Schwarzenegger')
+    A
+    AR
+    ARN
+    ARNO
+    ARNOL
+    ARNOLD
+    ARNOLD_ 
+    ARNOLD_S
+    ARNOLD_SC
+    ARNOLD_SCH
+    ARNOLD_SCHW
+    ARNOLD_SCHWA
+    ARNOLD_SCHWAR
+    ARNOLD_SCHWARZ
+    ARNOLD_SCHWARZE
+    ARNOLD_SCHWARZEN
+    ARNOLD_SCHWARZENE
+    ARNOLD_SCHWARZENEG
+    ARNOLD_SCHWARZENEGG
+    ARNOLD_SCHWARZENEGGE
+    ARNOLD_SCHWARZENEGGER
+
+    >>> nome_escada('gELO')
+    G
+    GE
+    GEL
+    GELO
+
+    >>> nome_escada('HeISenBErG')
+    H
+    HE
+    HEI
+    HEIS
+    HEISE
+    HEISEN
+    HEISENB
+    HEISENBE
+    HEISENBER
+    HEISENBERG
+"""
+def nome_escada(nome:str):
+    """Escreva aqui em baixo a sua solução"""
+    
