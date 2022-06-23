6b3b3967430279cb4b8db7d3c6c3ad8541e4b710
Gabriel Santos <gab.tec97@gmail.com>
2022-06-23 18:20:53 -0300

    Closes issue #76

---
+++ secao_05_exercicios_funcoes/ex_03_soma3.py
@@ -0,0 +1,13 @@
+"""
+Exercício 03 da seção de listas da Python Brasil:
+https://wiki.python.org.br/ExerciciosFuncoes
+
+Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos.
+
+  >>> soma(50, 100, 150)
+  300
+  >>> soma(1000, 2000, 3000)
+  6000
+  >>> soma(1, 2, 3)
+
+"""
