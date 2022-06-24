5e08358902b411163d801427d006e39554b3be7c
Sara Brangioni <sarabrangioni@gmail.com>
2022-06-24 11:01:43 -0300

    Teste finalizado

--- secao_06_exercicios_strings/ex_01_comparador_de_strings.py
+++ secao_06_exercicios_strings/ex_01_comparador_de_strings.py
@@ -15,9 +15,43 @@ As duas strings são de tamanhos diferentes.
 As duas strings possuem conteúdo diferente.
 
     >>> comparar('Brasil Hexa 2006', 'Brasil! Hexa 2006!')
-
+    String 1: Brasil Hexa 2006
+    String 2: Brasil! Hexa 2006!
+    Tamanho de "Brasil Hexa 2006": 16 caracteres
+    Tamanho de "Brasil! Hexa 2006!": 18 caracteres
+    As duas strings possuem tamanhos diferentes.
+    As duas strings possuem conteúdo diferentes.
+    >>> comparar('Hexa 2006', 'Hexa 2006')
+    String 1: Hexa 2006
+    String 2: Hexa 2006
+    Tamanho de "Hexa 2006": 9 caracteres
+    Tamanho de "Hexa 2006": 9 caracteres
+    As duas strings possuem tamanhos iguais.
+    As duas strings possuem conteúdo iguais.
+    >>> comparar('Hexa 2006', 'Hexa 2008')
+    String 1: Hexa 2006
+    String 2: Hexa 2008
+    Tamanho de "Hexa 2006": 9 caracteres
+    Tamanho de "Hexa 2008": 9 caracteres
+    As duas strings possuem tamanhos iguais.
+    As duas strings possuem conteúdo diferentes.
 """
 
 
 def comparar(s1: str, s2: str):
     """Escreva aqui em baixo a sua solução"""
+    print("String 1:", s1)
+    print("String 2:", s2)
+    tamanho1= len(s1)
+    tamanho2= len(s2)
+    print(f'Tamanho de "{s1}": {tamanho1} caracteres')
+    print(f'Tamanho de "{s2}": {tamanho2} caracteres')
+    if len(s1) == len(s2):
+        print("As duas strings possuem tamanhos iguais.")
+    else:
+        print("As duas strings possuem tamanhos diferentes.")
+    if s1 == s2:
+        print("As duas strings possuem conteúdo iguais.")
+    else:
+        print("As duas strings possuem conteúdo diferentes.")
+
