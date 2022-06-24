29af530e01b6dca261cbf7f0cee4c73253622d25
DaniDanis <104365695+DaniDanis@users.noreply.github.com>
2022-06-24 15:22:29 -0300

    Update ex_13_desenha_moldura.py

--- secao_05_exercicios_funcoes/ex_13_desenha_moldura.py
+++ secao_05_exercicios_funcoes/ex_13_desenha_moldura.py
@@ -8,7 +8,8 @@ Se valores fora da faixa forem informados, eles devem ser modificados para valor
 
 >>> from secao_05_exercicios_funcoes import ex_13_desenha_moldura
 >>> entradas = {'linhas':3,'colunas':7}
->>> ex_13_desenha_moldura.input = lambda k: entradas.pop()
+>>> ex_13_desenha_moldura.input = lambda k: entradas.get('linhas')
+>>> ex_13_desenha_moldura.input = lambda k: entradas.get('colunas')
 >>> ex_13_desenha_moldura.desenha_moldura()
 +-------+
 |       |
@@ -17,7 +18,8 @@ Se valores fora da faixa forem informados, eles devem ser modificados para valor
 +-------+
 >>> from secao_05_exercicios_funcoes import ex_13_desenha_moldura
 >>> entradas = {'linhas':0,'colunas':3}
->>> ex_13_desenha_moldura.input = lambda k: entradas.pop()
+>>> ex_13_desenha_moldura.input = lambda k: entradas.get('linhas')
+>>> ex_13_desenha_moldura.input = lambda k: entradas.get('colunas')
 >>> ex_13_desenha_moldura.desenha_moldura()
 O número mínimo de linhas é 1, a quantidade de linhas foi alterada para 1.
 +---+
@@ -25,7 +27,8 @@ O número mínimo de linhas é 1, a quantidade de linhas foi alterada para 1.
 +---+
 >>> from secao_05_exercicios_funcoes import ex_13_desenha_moldura
 >>> entradas = {'linhas':2,'colunas':0}
->>> ex_13_desenha_moldura.input = lambda k: entradas.pop()
+>>> ex_13_desenha_moldura.input = lambda k: entradas.get('linhas')
+>>> ex_13_desenha_moldura.input = lambda k: entradas.get('colunas')
 >>> ex_13_desenha_moldura.desenha_moldura()
 O número mínimo de colunas é 1, a quantidade de colunas foi alterada para 1.
 +-+
@@ -34,7 +37,8 @@ O número mínimo de colunas é 1, a quantidade de colunas foi alterada para 1.
 +-+
 >>> from secao_05_exercicios_funcoes import ex_13_desenha_moldura
 >>> entradas = {'linhas':25,'colunas':30}
->>> ex_13_desenha_moldura.input = lambda k: entradas.pop()
+>>> ex_13_desenha_moldura.input = lambda k: entradas.get('linhas')
+>>> ex_13_desenha_moldura.input = lambda k: entradas.get('colunas')
 >>> ex_13_desenha_moldura.desenha_moldura()
 O número máximo de linhas é 20, a quantidade de linhas foi alterada para 20.
 O número máximo de colunas é 20, a quantidade de colunas foi alterada para 20.
@@ -61,4 +65,3 @@ O número máximo de colunas é 20, a quantidade de colunas foi alterada para 20
 |                    |
 +--------------------+
 """
-
