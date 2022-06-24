3beda8f8fbf64f721fb2b60034f1a090d27f0f2b
DaniDanis <104365695+DaniDanis@users.noreply.github.com>
2022-06-24 15:01:33 -0300

    Update ex_13_desenha_moldura.py

--- secao_05_exercicios_funcoes/ex_13_desenha_moldura.py
+++ secao_05_exercicios_funcoes/ex_13_desenha_moldura.py
@@ -6,24 +6,36 @@ Desenha moldura. Construa uma função que desenhe um retângulo usando os carac
 Esta função deve receber dois parâmetros, linhas e colunas, sendo que o valor por omissão é o valor mínimo igual a 1 e o valor máximo é 20.
 Se valores fora da faixa forem informados, eles devem ser modificados para valores dentro da faixa de forma elegante.
 
->>> desenha_moldura (linhas = 3,  colunas = 7)
+>>> from secao_05_exercicios_funcoes import ex_13_desenha_moldura
+>>> entradas = {'linhas':3,'colunas':7}
+>>> ex_13_desenha_moldura.input = lambda k: entradas.pop()
+>>> ex_13_desenha_moldura.desenha_moldura()
 +-------+
 |       |
 |       |
 |       |
 +-------+
->>> desenha_moldura (linhas = 0,  colunas = 3)
+>>> from secao_05_exercicios_funcoes import ex_13_desenha_moldura
+>>> entradas = {'linhas':0,'colunas':3}
+>>> ex_13_desenha_moldura.input = lambda k: entradas.pop()
+>>> ex_13_desenha_moldura.desenha_moldura()
 O número mínimo de linhas é 1, a quantidade de linhas foi alterada para 1.
 +---+
 |   |
 +---+
->>> desenha_moldura (linhas = 2, colunas = 0)
+>>> from secao_05_exercicios_funcoes import ex_13_desenha_moldura
+>>> entradas = {'linhas':2,'colunas':0}
+>>> ex_13_desenha_moldura.input = lambda k: entradas.pop()
+>>> ex_13_desenha_moldura.desenha_moldura()
 O número mínimo de colunas é 1, a quantidade de colunas foi alterada para 1.
 +-+
 | |
 | |
 +-+
->>> desenha_moldura (linhas = 25, colunas = 30)
+>>> from secao_05_exercicios_funcoes import ex_13_desenha_moldura
+>>> entradas = {'linhas':25,'colunas':30}
+>>> ex_13_desenha_moldura.input = lambda k: entradas.pop()
+>>> ex_13_desenha_moldura.desenha_moldura()
 O número máximo de linhas é 20, a quantidade de linhas foi alterada para 20.
 O número máximo de colunas é 20, a quantidade de colunas foi alterada para 20.
 +--------------------+
