96c3ced9ac909e34450bfc57b845af342a9d53ee
Marcela Lima <marcelalima.df@gmail.com>
2022-06-24 16:37:16 -0300

    solucao ex 05 #98

---
+++ secao_05_exercicios_funcoes/ex_05_soma_imposto.py
@@ -0,0 +1,30 @@
+"""
+Exercício 05 da seção de listas da Python Brasil:
+https://wiki.python.org.br/ExerciciosFuncoes
+
+Faça um programa com uma função chamada somaImposto. A função possui dois parâmetros formais: taxaImposto, que é a quantia de imposto sobre vendas expressa em porcentagem e custo, que é o custo de um item antes do imposto. A função “altera” o valor de custo para incluir o imposto sobre vendas.
+
+    >>> somaImposto(20, 55)
+    66
+    >>> somaImposto(30.5, 95.7)
+    125
+    >>> somaImposto(-10, 90)
+    A taxa de Imposto não pode ser negativa
+    >>> somaImposto(10, -90)
+    O custo não pode ser negativo
+"""
+import math
+def somaImposto(taxaImposto, custo):
+    """escreva aqui em baixo sua solução"""
+    
+    print(somaAlterada(taxaImposto, custo))
+
+def somaAlterada(taxaImposto, custo):
+    #cálculo do valor final com imposto
+    if taxaImposto < 0:
+        return 'A taxa de Imposto não pode ser negativa'
+    elif custo < 0:
+        return 'O custo não pode ser negativo'
+    else:
+        valor_imposto = custo + (custo*taxaImposto/100)
+        return f'{valor_imposto:.0f}'
