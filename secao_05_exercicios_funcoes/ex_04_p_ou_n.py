ca6110b88e58c0fb5a8582463921260eb43d6443
Ingrid Caroline <ingridcaroline3012@gmail.com>
2022-06-24 17:26:57 -0300

    docteste finalizado

---
+++ secao_05_exercicios_funcoes/ex_04_p_ou_n.py
@@ -0,0 +1,21 @@
+"""
+Exercício 04 da seção de funções da Python Brasil:
+https://wiki.python.org.br/ExerciciosFuncoes
+
+Faça um programa, com uma função que necessite de um argumento. 
+A função retorna o valor de caractere ‘P’, se seu argumento for positivo, 
+e ‘N’, se seu argumento for zero ou negativo.
+
+    >>> positivo_negativo(2)
+    'P'
+    >>> positivo_negativo(-1)
+    'N'
+    >>> positivo_negativo(0)
+    'N'
+    >>> positivo_negativo(-100)
+    'N'
+    >>> positivo_negativo(90)
+    'P'
+    
+"""
+
