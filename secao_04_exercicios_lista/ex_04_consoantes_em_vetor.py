19be1d18d48f33875567fce8911e475026959488
Ana Clara Coelho <ana.ccoelho_16@outlook.com>
2022-06-24 11:11:11 -0300

    Feito teste do exercício 4 da seção 04

---
+++ secao_04_exercicios_lista/ex_04_consoantes_em_vetor.py
@@ -0,0 +1,19 @@
+"""
+Exercício 04 da seção de listas da Python Brasil:
+https://wiki.python.org.br/ExerciciosListas
+
+Faça um Programa que leia um vetor de 10 caracteres, e diga quantas consoantes foram lidas. Imprima as consoantes.
+
+    >>> imprimir_consoantes('consoantes')
+    'A palavra "consoantes" possui 6 consoantes: c, n, s, t'
+    >>> imprimir_consoantes('programador')
+    'Escreva uma palavra com 10 caracteres'
+    >>> imprimir_consoantes('amor')
+    'Escreva uma palavra com 10 caracteres'
+    >>> imprimir_consoantes('engenheiro')
+    'A palavra "engenheiro" possui 5 consoantes: n, g, h, r'
+"""
+
+
+def imprimir_consoantes(palavra: str):
+    """Escreva aqui embaixo a sua solução"""
