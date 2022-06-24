c298fee2bf2d29292565a45a316ff6194c9f43ab
Bruna Andrade <brunansandrade@gmail.com>
2022-06-24 18:04:19 -0300

    melhorando espacamento solucao exercicio 06 da secao 06 #55

--- secao_06_exercicios_strings/ex_06_data_por_extenso.py
+++ secao_06_exercicios_strings/ex_06_data_por_extenso.py
@@ -2,20 +2,26 @@
 Exercício 06 da seção de strings da Python Brasil:
 https://wiki.python.org.br/ExerciciosComStrings
 Faça um programa que solicite a data de nascimento (dd/mm/aaaa) do usuário e imprima a data com o nome do mês por extenso.
+
     >>> data_por_extenso(32, 13, 1975)
     Data inválida.
+
     >>> data_por_extenso(20, 1, 2000)
     Data de Nascimento: 20/01/2000
     Você nasceu em 20 de Janeiro de 2000.
+
     >>> data_por_extenso(31, 8, 1950)
     Data de Nascimento: 31/08/1950
     Você nasceu em 31 de Agosto de 1950.
+
     >>> data_por_extenso(2, 2, 1994)
     Data de Nascimento: 02/02/1994
     Você nasceu em 2 de Fevereiro de 1994.
+
     >>> data_por_extenso(5, 10, 1989)
     Data de Nascimento: 05/10/1989
     Você nasceu em 5 de Outubro de 1989.
+
 """
 
 
