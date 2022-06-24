cd148f60ce68cd989f0fcb920de849a3fcfc1c5d
Daniel Dantas da Silva <daniel.dantas.98@gmail.com>
2022-06-24 10:58:22 -0300

    Algumas modificacoes

--- secao_05_exercicios_funcoes/ex_13_desenha_moldura.py
+++ secao_05_exercicios_funcoes/ex_13_desenha_moldura.py
@@ -6,44 +6,47 @@ Desenha moldura. Construa uma função que desenhe um retângulo usando os carac
 Esta função deve receber dois parâmetros, linhas e colunas, sendo que o valor por omissão é o valor mínimo igual a 1 e o valor máximo é 20.
 Se valores fora da faixa forem informados, eles devem ser modificados para valores dentro da faixa de forma elegante.
 
->>> desenha_moldura (5, 7)
-+-----+
-|     |
-|     |
-|     |
-+-----+
->>> desenha_moldura (0, 3)
-Não é possivel construir uma moldura sem linhas, a quantidade de linhas foi alterada para 1.
+>>> desenha_moldura (linhas = 3,  colunas = 7)
++-------+
+|       |
+|       |
+|       |
++-------+
+>>> desenha_moldura (linhas = 0,  colunas = 3)
+O número mínimo de linhas é 1, a quantidade de linhas foi alterada para 1.
++---+
+|   |
++---+
+>>> desenha_moldura (linhas = 2, colunas = 0)
+O número mínimo de linhas é 1, a quantidade de colunas foi alterada para 1.
 +-+
->>> desenha_moldura (5, 0)
-Não é possivel construir uma moldura sem colunas, a quantidade de colunas foi alterada para 1.
-+
-|
-|
-|
-+
->>> desenha_moldura (25, 30)
-O numero maximo de linhas é 20, a quantidade de linhas foi alterado para 20.
-O numero maximo de colunas é 20, a quantidade de colunas foi alterado para 20.
-+------------------+
-|                  |
-|                  |
-|                  |
-|                  |
-|                  |
-|                  |
-|                  |
-|                  |
-|                  |
-|                  |
-|                  |
-|                  |
-|                  |
-|                  |
-|                  |
-|                  |
-|                  |
-|                  |
-+------------------+
+| |
+| |
++-+
+>>> desenha_moldura (linhas = 25, colunas = 30)
+O número máximo de linhas é 20, a quantidade de linhas foi alterada para 20.
+O número máximo de colunas é 20, a quantidade de colunas foi alterada para 20.
++--------------------+
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
++--------------------+
 """
 
