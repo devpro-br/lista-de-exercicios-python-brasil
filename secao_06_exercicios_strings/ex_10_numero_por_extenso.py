b0b8e783b8ccb2a354e5caa4e0bbff5be19f2362
Glória Brito <gloriafbrito@gmail.com>
2022-06-25 15:59:25 -0300

    contribuição do exercício 10 da seção de strings

---
+++ secao_06_exercicios_strings/ex_10_numero_por_extenso.py
@@ -0,0 +1,24 @@
+"""
+Exercício 10 da seção de Strings da Python Brasil:
+https://wiki.python.org.br/ExerciciosComStrings
+
+Faça um Programa que solicite ao usuário a digitação de um número até 99 e imprima-o na tela por extenso.
+
+    >>> numero_por_extenso(4)
+    quatro
+    >>> numero_por_extenso(2)
+    dois
+    >>> numero_por_extenso(99)
+    noventa e nove
+    >>> numero_por_extenso(75)
+    setenta e cinco
+    >>> numero_por_extenso(53)
+    cinquenta e três
+    >>> numero_por_extenso(7)
+    sete
+    
+
+""" 
+
+def numero_por_extenso(n1: int):
+    """Escreva aqui em baixo a sua solução"""
