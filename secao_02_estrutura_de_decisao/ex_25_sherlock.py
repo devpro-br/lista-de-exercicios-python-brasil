b9224d901bc91464439dd85e0447aa25a240eb1c
Fabio Freitas <fabiofsrj@gmail.com>
2022-06-26 13:32:36 -0300

    inclusão da resoluçao do exercício 25 da lista de estrutura de decisão.

--- secao_02_estrutura_de_decisao/ex_25_sherlock.py
+++ secao_02_estrutura_de_decisao/ex_25_sherlock.py
@@ -32,4 +32,30 @@ Caso contrário, ele será classificado como "Inocente".
 
 
 def investigar(telefonou: str, estava_no_local: str, mora_perto: str, devia: str, trabalhou: str, ):
-    """Escreva aqui em baixo a sua solução"""
+    perguntas = [ "Telefonou para a vítima? ", "Esteve no local do crime? ",
+    "Mora perto da vítima? ", "Devia para a vítima? ", "Já trabalhou com a vítima? "]
+    respostas_positivas = 0
+
+    for a in range(5):
+        resposta = ''
+        while resposta != 'sim' and resposta != 'não':
+            print(perguntas[a], end='')
+            resposta = input().lower()
+            if resposta == 'sim':
+                respostas_positivas +=1
+
+
+    if respostas_positivas == 5:
+        classificacao = 'Assassino'
+
+    elif respostas_positivas == 3 or respostas_positivas == 4:
+        classificacao = 'Cúmplice'
+
+    elif respostas_positivas == 2:
+        classificacao = 'Suspeito'
+
+    elif respostas_positivas < 2:
+        classificacao = 'Inocente'
+
+    print (classificacao)
+
