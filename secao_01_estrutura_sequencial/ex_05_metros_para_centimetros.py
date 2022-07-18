6ede3d8ff593da6623a4909bf0002c8957b059ef
Fabio F. de Souza <fabiofsrj@hotmail.com>
2022-07-17 21:57:37 -0300

    inclusao de variáveis na função doctest

--- secao_01_estrutura_sequencial/ex_05_metros_para_centimetros.py
+++ secao_01_estrutura_sequencial/ex_05_metros_para_centimetros.py
@@ -15,12 +15,16 @@ Faça um Programa que converta metros para centímetros.
 """
 
 
-def converter_metros_para_centimetros(metro):
-    while True:
-        try:
-            #metro = float(input('Informe a medida em metros a ser convertida em centimetros: '))
-            centimetro = metro * 100        
-            print(f'Transformando para centímetros dá {centimetro} cm')
-            break # Adiciondo para só perguntar uma vez e não prejudicar doctest.
-        except ValueError:
-            print('Não informou um número!!!')
+def converter_metros_para_centimetros(x):
+    return x
+
+
+while True:
+    try:
+        metro = converter_metros_para_centimetros(1)
+        #metro = float(input('Informe a medida em metros a ser convertida em centimetros: '))
+        centimetro = metro * 100        
+        print(f'Transformando para centímetros dá {centimetro} cm')
+        break # Adiciondo para só perguntar uma vez e não prejudicar doctest.
+    except ValueError:
+        print('Não informou um número!!!')
