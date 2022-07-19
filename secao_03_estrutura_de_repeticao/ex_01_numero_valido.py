bc43ea0855e55c61fe8f11283a60d2d1d31d3244
Fabio F. de Souza <fabiofsrj@hotmail.com>
2022-07-19 19:47:03 -0300

    resposta exe 01 lista de repetição

--- secao_03_estrutura_de_repeticao/ex_01_numero_valido.py
+++ secao_03_estrutura_de_repeticao/ex_01_numero_valido.py
@@ -32,16 +32,17 @@ até que o usuário informe um valor válido.
 """
 
 
-def obter_numero_valido():
-    while True:
-        try:
-            nota = 11
-            while nota < 0 or nota > 10:
-                nota_str = input('Entre com uma nota entre 0 e 10: ')
-                if not nota_str in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']:
-                    print(f'Número inválido: {nota_str}')
-                nota = int(nota_str)
-            print(nota)
+def obter_numero_valido(valor:str):
+    try:
+        nota = int(valor)
+        if valor < 0 or valor > 10:
+            mensagem = f"Número inválido: {valor}"
+        else:
+            mensagem = nota
+    except ValueError:
+        mensagem = f"Número inválido: {valor}"
 
-        except ValueError:
-            break
+    return print(mensagem)
+
+#nota = input('Entre com uma nota entre 0 e 10: ')
+obter_numero_valido(-1)
