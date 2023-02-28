e2bf9703e542eac041fcc99190cb9e149c4766aa
renzon <renzon@gmail.com>
2023-02-28 11:15:20 -0300

    Feito exercício 03 da lista sobre lista de Python

---
+++ secao_04_exercicios_lista/ex_03_media_4_notas.py
@@ -0,0 +1,16 @@
+"""
+Exercício 03 da seção de listas da Python Brasil:
+https://wiki.python.org.br/ExerciciosListas
+
+Faça um Programa que receba 4 notas, mostre as notas e a média na tela. 
+
+    >>> mostrar_notas_e_media([0, 1, 2, 3])
+    'Notas: 0, 1, 2, 3. Média: 1.5'
+    >>> mostrar_notas_e_media([10, 14, 16, 26])
+    'Notas: 10, 14, 16, 26. Média: 16.5'
+   
+"""
+
+
+def mostrar_notas_e_media(inteiros: list) -> str:
+    """Escreva aqui em baixo a sua solução"""
