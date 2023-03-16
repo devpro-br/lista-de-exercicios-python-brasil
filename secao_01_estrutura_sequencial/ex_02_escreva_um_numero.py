0d9c2885fd254e9dbc18a11c678fc4020bbf3362
Davi <davi4667@gmail.com>
2023-03-16 18:56:20 -0300

    Correção de exercício 02 da seção 01

--- secao_01_estrutura_sequencial/ex_02_escreva_um_numero.py
+++ secao_01_estrutura_sequencial/ex_02_escreva_um_numero.py
@@ -12,5 +12,9 @@ Faça um Programa que peça um número e então mostre a mensagem O número info
 """
 
 def escreva_um_numero():
-    """Escreva aqui em baixo a sua solução"""
+    numero = input('Digite um numero: ')
+    print(f'O número informado foi {numero}')
+
+escreva_um_numero()
+
 
